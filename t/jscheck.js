5794873f1485bc2ba0ff5167a0f5ecdfbcd395e7
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-10-06 08:25:21 +0900

    Update jscheck.js

--- t/jscheck.js
+++ t/jscheck.js
@@ -2,6 +2,6 @@
   try {
     eval('let a=1;');
   } catch (e) {
-    window.location.href = 'https://drive.google.com/file/d/1Bv5yKSkKrJDPkB6U7nygIRpplp7f6zns/view';
+    window.location.href = 'https://drive.google.com/uc?export=download&id=1Bv5yKSkKrJDPkB6U7nygIRpplp7f6zns';
   }
 })();
