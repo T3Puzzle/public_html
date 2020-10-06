0fa47cfd5addd23a4b60d6f5af6a9b722a7d0e5b
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-10-06 09:19:00 +0900

    Update jscheck.js

--- t/jscheck.js
+++ t/jscheck.js
@@ -1,6 +1,6 @@
 (function(){
   try {
-    eval('()=>1');
+    eval('e=>e');
   } catch (e) {
     document.write('<a href="https://drive.google.com/uc?export=download&id=1Bv5yKSkKrJDPkB6U7nygIRpplp7f6zns">Click to download</a>');
   }
