de6ba27a4c87c3978ba096997b15e3a3faca95ff
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-10-06 09:05:13 +0900

    Delete test.js

--- t/test.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  try {
-    eval('let a=1;');
-  } catch (e) {
-    document.body.innerHTML = '<a href="https://drive.google.com/uc?export=download&id=1Bv5yKSkKrJDPkB6U7nygIRpplp7f6zns">Click to download</a>';
-  }
-})();
