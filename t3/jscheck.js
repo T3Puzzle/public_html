6feb7615d85d785f99fcf845f12d89f191c131a8
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-11-17 06:45:57 +0900

    Create jscheck.js

---
+++ t3/jscheck.js
@@ -0,0 +1,7 @@
+(function(){
+  try {
+    eval('e=>e');
+  } catch (e) {
+    document.write('<a href="https://drive.google.com/uc?export=download&id=1Bv5yKSkKrJDPkB6U7nygIRpplp7f6zns">Click to download</a>');
+  }
+})();
