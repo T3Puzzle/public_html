092b98b8f89a127b6e108ed80a729d8365e9d0a6
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-11-17 16:14:13 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -1,14 +1,16 @@
 (()=>{
 let config='svgconfig';
-let size = 80;
+let size = 100;
 let text = `
-チャットは全員宛へ / 
+<button>
+チャットは全員宛へ<br/> 
 <a href="https://forms.gle/KGSthgPT5zvkwbVn8">アンケート</a>
+</button>
 `;
 document.addEventListener('DOMContentLoaded',()=>{
   let output = document.body.querySelector(`output.${config}`);
   output.insertAdjacentHTML('beforeend',`
-<span class="${name}" style="word-wrap: break-word;text-decoration:none;vertical-align:0.2em;font-size:${size}%;margin-right:0.5em;">${text}</span>
+<span class="${name}" style="word-wrap: break-word;text-decoration:none;vertical-align:0.2em;font-size:${size}%;margin-right:0.2em;">${text}</span>
 `);
 });
 })();
