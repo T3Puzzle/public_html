71306ea9068a1dc28d2f3b274220bd3c82b6f688
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-11-18 17:36:28 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -2,7 +2,8 @@
 let config='svgconfig';
 let size = 100;
 let text = `
-<a href="https://forms.gle/KGSthgPT5zvkwbVn8">アンケート</a>
+<a href="https://forms.gle/KGSthgPT5zvkwbVn8">アンケート</a> / 
+<a href="./upload.html">画像</a> 
 `;
 document.addEventListener('DOMContentLoaded',()=>{
   let output = document.body.querySelector(`output.${config}`);
