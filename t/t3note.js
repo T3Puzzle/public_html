61e643ab579f430fe34bf0a73132dfeda0e42ed9
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-11-21 09:38:24 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -1,8 +1,13 @@
 (()=>{
 let config='svgconfig';
 let size = 100;
+let artist = localStorage.getItem('artist');
+if (!artist) {
+   artist = '';
+}
+let url = 'https://docs.google.com/forms/d/e/1FAIpQLScO2CcSVsqd1a1LYUdVUA3oamnmEHkzee3seSs0v3IqCjvD1Q/viewform?usp=pp_url&entry.241675505='+artist;
 let text = `
-<a href="https://forms.gle/KGSthgPT5zvkwbVn8">アンケート</a> / 
+<a href="${url}">アンケート</a> / 
 <a href="./upload.html">写真</a> 
 `;
 document.addEventListener('DOMContentLoaded',()=>{
