98abd0ae281fff89fbf5694b0000fde86d3a9f11
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-11-23 08:30:15 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -5,7 +5,7 @@ let artist = localStorage.getItem('artist');
 if (!artist) {
    artist = '';
 }
-let url = 'https://docs.google.com/forms/d/e/1FAIpQLScO2CcSVsqd1a1LYUdVUA3oamnmEHkzee3seSs0v3IqCjvD1Q/viewform?usp=pp_url&entry.241675505='+artist;
+let url = 'https://docs.google.com/forms/d/e/1FAIpQLSfWOXB9V0O8ThOcjhmg7RngBs8NT1a2hLElKAEE8-22uzB7sQ/viewform?usp=pp_url&entry.241675505='+artist;
 let text = `
 <a href="${url}">アンケート</a> / 
 <a href="./upload.html">写真</a> /
