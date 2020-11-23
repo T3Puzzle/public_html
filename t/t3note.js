66d05657e1d58406f2f15a3492220430b6a79058
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-11-23 12:30:35 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -5,11 +5,11 @@ let artist = localStorage.getItem('artist');
 if (!artist) {
    artist = '';
 }
-let url = 'https://docs.google.com/forms/d/e/1FAIpQLSfWOXB9V0O8ThOcjhmg7RngBs8NT1a2hLElKAEE8-22uzB7sQ/viewform?usp=pp_url&entry.241675505='+artist;
+let url = 'https://www.t3puzzle.com';
+let tw = 'https://twitter.com/t3puzzle';
 let text = `
-<a href="${url}">アンケート</a> / 
-<a href="./upload.html">写真</a> /
-<a href="./confirm.html">？</a>
+<a href="${url}">パズル購入</a> / 
+<a href="${tw}">作品例</a>
 `;
 document.addEventListener('DOMContentLoaded',()=>{
   let output = document.body.querySelector(`output.${config}`);
