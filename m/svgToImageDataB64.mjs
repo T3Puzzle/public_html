dc052a1e21c60db1bb58018058aecb1de2dbed50
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-11-26 12:44:47 +0900

    Add files via upload

---
+++ m/svgToImageDataB64.mjs
@@ -0,0 +1,58 @@
+export function __svgToImageDataB64(svgElement, callback) {
+  let canvas = document.createElement('canvas');
+  let svgwidth = svgElement.width.baseVal.value;
+  let svgheight = svgElement.height.baseVal.value;
+  canvas.width = svgwidth;
+  canvas.height = svgheight;
+  let ctx = canvas.getContext('2d');
+  let image = new Image();
+  image.addEventListener('load',()=>{
+    ctx.drawImage(image, 0, 0, svgwidth, svgheight);
+    let type = 'image/png';
+    toB64(canvas,callback,type);
+  });
+  image.addEventListener('error',e=>console.error(e));
+  var svgData = new XMLSerializer().serializeToString(svgElement);
+  // use of btoa
+  image.src = 'data:image/svg+xml;charset=utf-8;base64,' + btoa(svgData);
+}
+function toB64(canvas,callback,type) {
+  let dataurl = canvas.toDataURL(type);
+  let b64 = dataurl.split(',')[1];
+  callback(getFormData(b64));
+}
+function getFormData (data) {
+  return {
+    filename: getFileName(),
+    type: 'image/png',
+    touch:  ('ontouchend' in document)?'true':'',
+    content: data
+  };
+}
+function getFileName() {
+  let pathname = document.location.pathname;
+  let name = '';
+  if (/([^\/]+)$/.exec(pathname)) {
+    pathname = RegExp.$1;
+    if (/^([^\?#.]+)/.exec(pathname)) {
+       name = RegExp.$1;
+    }
+  }
+  let artist = localStorage.getItem('artist');
+  if (!(artist && artist.toString().trim().length>0)) {
+    artist = Math.random().toString(32).substring(2);
+    localStorage.setItem('artist',artist);
+  }
+  return artist+'_'+name+getDateStr()+'.png';
+}
+function getDateStr() {
+  let date = new Date();
+  let millisec = date.getMilliseconds();
+  if (millisec<10) {
+    millisec = '00'+millisec;
+  } else if (millisec<100) {
+    millisec = '0'+millisec;
+  }
+  let options = { year: 'numeric', month: '2-digit', day: '2-digit', hour:'2-digit', minute:'2-digit', second:'2-digit'};
+  return date.toLocaleString('ja-JP',options).replace(/(\/|:)/g,'').replace(/ /g,'_')+'_'+millisec;
+}
