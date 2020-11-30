5dd69c38334cd4c67137f2c09a00de6279a52370
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-12-01 07:25:59 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -7,9 +7,11 @@ if (!artist) {
 }
 let url = 'https://www.t3puzzle.com';
 let tw = 'https://twitter.com/t3puzzle';
+let yt = 'https://www.youtube.com/watch?v=9J_gAmeo-Gk&t=16m05s';
 let text = `
 <a target="_blank" href="${url}">パズル購入</a> / 
-<a target="_blank" href="${tw}">作品例</a>
+<a target="_blank" href="${tw}">作品例</a> /
+<a target="_blank" href="${yt}">説明動画</a>
 `;
 document.addEventListener('DOMContentLoaded',()=>{
   let output = document.body.querySelector(`output.${config}`);
