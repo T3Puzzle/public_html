0fb4f4021b25560809b576d3c87b6d4feaddc813
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2020-12-07 22:51:15 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -9,8 +9,8 @@ let url = 'https://www.tessellation.jp/t3dream/r';
 let sub = 'https://www.t3puzzle.com/r/u';
 let text = `
 <a target="_blank" href="${url}">コンテスト</a> / 
-<a href="javascript:document.querySelector('a.pngview').click();">画像に保存</a> /
-<a href="${sub}">画像を応募</a>
+<a href="javascript:document.querySelector('a.pngview').click();">1. 画像に保存</a> /
+<a href="${sub}">2. 保存画像を応募</a>
 `;
 document.addEventListener('DOMContentLoaded',()=>{
   let output = document.body.querySelector(`output.${config}`);
