8a999e95a2871bfa01f1196d8ea0c1bbf402a938
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-05-26 10:12:22 +0900

    Update t3note.js

--- c/t3note.js
+++ c/t3note.js
@@ -6,11 +6,11 @@ if (!artist) {
    artist = '';
 }
 let url = 'https://www.tessellation.jp/online/t3web';
-let sub = 'https://www.t3puzzle.com/';
+let sub = './upload.html';
 let text = `
 <a target="_blank" href="${url}">使い方</a> / 
 <a href="#" onclick="javascript:document.querySelector('a.pngview').click();return false;">画像に保存</a> /
-<a target="_blank" href="${sub}"> ウェブショップ</a>
+<a target="_blank" href="${sub}"> 画像をアップロード</a>
 `;
 document.addEventListener('DOMContentLoaded',()=>{
   let output = document.body.querySelector(`output.${config}`);
