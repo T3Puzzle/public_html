45485c83562c6a46a3b2c0467ec5b2805afcc3e2
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-07-22 20:52:06 +0900

    Update t3note.js

--- p/t3note.js
+++ p/t3note.js
@@ -1,15 +1,18 @@
 (()=>{
 let config='svgconfig';
-let size = 150;
+let size = 120;
 let artist = localStorage.getItem('artist');
 if (!artist) {
    artist = '';
 }
 let url = 'https://www.tessellation.jp/online/t3web';
+let url2 = 'https://www.tessellation.jp/t3summer';
 let text = `
 <a target="_blank" href="${url}">使い方</a> / 
-<a href="#" onclick="javascript:document.querySelector('a.pngview').click();return false;">T3サマー事前登録へ</a>
+<a target="_blank" href="${url2}">コンテスト</a> / 
+<a href="#" onclick="javascript:document.querySelector('a.pngview').click();return false;">登録情報記入へ</a>
 `;
+
 document.addEventListener('DOMContentLoaded',()=>{
   let output = document.body.querySelector(`output.${config}`);
   output.insertAdjacentHTML('beforeend',`
