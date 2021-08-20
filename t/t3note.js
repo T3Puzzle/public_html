78c56a6858be689fd52c783e8d582d746f5fd3db
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-08-21 06:38:29 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -7,8 +7,10 @@ if (!artist) {
 }
 let url = 'https://www.tessellation.jp/online/t3web';
 let sub = 'https://www.t3puzzle.com/r/u?pop';
+let quest = 'https://docs.google.com/forms/d/e/1FAIpQLSev2VmJmDiQIW8K_XEQYNMk0XQJDrrzAs0IeFXDeXuqm1Ry_Q/viewform?usp=sf_link?usp=pp_url&entry.241675505='+artist;
 let text = `
 <a target="_blank" href="${url}">使い方</a> / 
+<a target="_blank" href="${quest}">未来</a> / 
 <a href="#" onclick="javascript:document.querySelector('a.pngview').click();return false;">画像に保存</a> /
 <a target="_blank" href="${sub}">画像を応募</a>
 `;
