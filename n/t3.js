277ef9ecbd48af1dde1d4faa43eafd98337a9856
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-09-14 21:53:47 +0900

    Update t3.js

--- n/t3.js
+++ n/t3.js
@@ -1,5 +1,7 @@
 (() => {
   let posHash = {};
+  let basestate = {};
+  let dirstate = {};
   let prevX = 0;
   let prevY = 0;
   initHtml();
@@ -9,20 +11,26 @@
     "tapspace-grid",
     class extends HTMLElement {
       connectedCallback() {
-        let template = this.querySelector("template");
-        if (!template) {
-          return;
-        }
         let div = document.createElement("div");
         div.style = getCommonStyle();
         div.insertAdjacentHTML("beforeend", getTriangleStyle());
-        this.attachShadow({ mode: "open" }).append(div);
+        let shadow = this.attachShadow({ mode: "open" });
+        shadow.append(div);
         view.mount(div);
-        process(template.innerHTML, view);
+        let html = ` 
+    <div style="transform-origin:50px 28.867513px;transform:scale(0.97);">
+         <div class="tr  white">
+     </div>
+ <div style="transform-origin:0px 0px;transform:scale(0.50);">
+     <div class="tr  blue">
+     </div>
+      </div>
+    </div>`;
+        process(html, view, shadow);
       }
     }
   );
-  function process(tile, view) {
+  function process(tile, view, shadow) {
     const XSIZE = 100;
     const YSIZE = 86.60254;
     const WX = 8;
@@ -43,53 +51,77 @@
 
     var touchmode = { translate: true, tap: true };
 
-     let gstart = function(pp,px) {
-       console.log(pp._T);
-            px.bringToFront();
-            deleteHash(pp,px);
-           prevX = pp._T.tx;
-           prevY = pp._T.ty;
-    }
-    let gend = function (pp,px) {
-            let dx = -pp._T.tx;
-            let dy = -pp._T.ty;
-            pp.snap(px.atMid(), grid);
-            dx += pp._T.tx;
-            dy += pp._T.ty;
-            let norm = Math.sqrt(dx * dx + dy * dy);
-            console.log(norm);
-            
-            if (norm>50){
-              console.log(pp._T);
-              
-              // revert first
-              //pp.rotate(px.at(0,0), Math.PI/2);
-              toggleDownsideup(pp);
-              console.log(pp._T);
-              pp.snap(px.atMid(), grid);
-              console.log(pp._T);
-            }
-            
-            if (checkHash(pp,px)) {
-              console.log("dup"+genKey(pp,px));
-              pp._T.tx = prevX;
-              pp._T.ty = prevY;
-              pp.snap(px.atMid(), grid);
-              
-              // TODO: try rotate again
-              
-            } else {
-              console.log("safe"+genKey(pp,px));
-            }
-            setHash(pp,px);
-    }
-    let tapHandler = function (ev, px) {
+    let gstart = function (pp, px) {
+      // console.log(pp._T);
+      //px.bringToFront();
+      deleteHash(pp, px);
+      prevX = pp._T.tx;
+      prevY = pp._T.ty;
+    };
+    let gend = function (pp, px) {
+      let dx = -pp._T.tx;
+      let dy = -pp._T.ty;
+      pp.snap(px.atMid(), grid);
+      dx += pp._T.tx;
+      dy += pp._T.ty;
+      let norm = Math.sqrt(dx * dx + dy * dy);
+      console.log(norm);
+
+      if (norm > 50) {
+        // console.log(pp._T);
+
+        // revert first
+        //pp.rotate(px.at(0,0), Math.PI/2);
+        toggleDownsideup(pp);
+        // console.log(pp._T);
+        pp.snap(px.atMid(), grid);
+        // console.log(pp._T);
+      }
+
+      if (checkHash(pp, px)) {
+        console.log("dup" + genKey(pp, px));
+        pp._T.tx = prevX;
+        pp._T.ty = prevY;
+        pp.snap(px.atMid(), grid);
+
+        // TODO: try rotate again
+      } else {
+        console.log("safe" + genKey(pp, px));
+      }
+      setHash(pp, px);
+    };
+    let tapHandler = function (ev, px, idx, px3) {
+      let id = ev.item.id;
+      if (idx !== 3) {
+        if (id in basestate) {
+          if (dirstate[id] === idx) {
+            basestate[id] = (basestate[id] + 1) % 2;
+          } else {
+            dirstate[id] = idx;
+          }
+        } else {
+          basestate[id] = 0;
+          dirstate[id] = 0;
+        }
+      }
+      console.log(
+        "idx=" + idx + "@" + id + "=" + basestate[id] + " " + dirstate[id]
+      );
+      if (idx!==3) {
+      px3.resetTransform();
+      px3.rotate(
+                px3.at(XSIZE / 2, YSIZE / 3),
+                [0, (Math.PI * 2) / 3, (-Math.PI * 2) / 3][dirstate[id]]
+              );
+        
+      }
+/*
       px.resetTransform();
-      // TODO:
       px.rotate(
-            px.at(XSIZE / 2, YSIZE / 3),
-            pickRandom([0, (Math.PI * 2) / 3, (-Math.PI * 2) / 3])
-          );
+        px.at(XSIZE / 2, YSIZE / 3),
+        [0, (Math.PI * 2) / 3, (-Math.PI * 2) / 3][dirstate[id]]
+      );
+      */
     };
     for (let i = 0; i < WX; i++) {
       for (let j = 0; j < WY; j++) {
@@ -106,27 +138,70 @@
           if (i - j < -WY / 2) {
             continue;
           }
-          var pp = new tapspace.SpaceGroup(g);
-          let px = new tapspace.SpaceHTML(tile, pp);
-          px.setSize(0, 0);
-          pp.translate(px.atMid(), grid.at(i, j));
-          if (k===1) {
-            pp.rotate(px.at(50,0),Math.PI);
-          }
-          px.rotate(
-            px.at(XSIZE / 2, YSIZE / 3),
-            pickRandom([0, (Math.PI * 2) / 3, (-Math.PI * 2) / 3])
-          );
-          setHash(pp,px);
 
-          let touch = new tapspace.Touchable(view, pp);
-          touch.start(touchmode);
-          touch.on("gesturestart", ((pp,px)=>() => gstart(pp,px)
-          )(pp,px));
-          touch.on("gestureend", ((pp,px)=>()=>gend(pp,px)
-          )(pp,px));
-          touch.on("tap", (px=>(e)=>tapHandler(e,px)
-          )(px));
+          let tiles = getTiles();
+          var pp = new tapspace.SpaceGroup(g);
+          var pq = new tapspace.SpaceGroup(pp);
+          let px3 = new tapspace.SpaceHTML(tile, pq);
+
+          let px0 = new tapspace.SpaceHTML(tiles[0], pq);
+          let px1 = new tapspace.SpaceHTML(tiles[1], pq);
+          let px2 = new tapspace.SpaceHTML(tiles[2], pq);
+
+          const { rndi, rndv } = pickRandom([
+            0,
+            (Math.PI * 2) / 3,
+            (-Math.PI * 2) / 3,
+            0,
+            (Math.PI * 2) / 3,
+            (-Math.PI * 2) / 3
+          ]);
+          let idx = 0;
+          [px0, px1, px2, px3].map((px) => {
+            px.setSize(0, 0);
+            if (idx === 3) {
+              pp.translate(px.atMid(), grid.at(i, j));
+              if (k === 1) {
+                pp.rotate(px.at(50, 0), Math.PI);
+              }
+
+              let touch = new tapspace.Touchable(view, pp);
+              touch.start(touchmode);
+              touch.on(
+                "gesturestart",
+                ((pp, px) => () => gstart(pp, px))(pp, px)
+              );
+              touch.on("gestureend", ((pp, px) => () => gend(pp, px))(pp, px));
+              touch.on(
+                "tap",
+                ((px, idx) => (e) => tapHandler(e, px, idx))(px, idx)
+              );
+              
+              px.rotate(
+                px.at(XSIZE / 2, YSIZE / 3),
+                rndv
+              );
+              
+              dirstate[pp.id] = rndi % 3;
+              basestate[pp.id] = Math.floor(rndi / 3);
+              /*
+              pq.resetTransform();
+              pq.rotate(
+                px.at(XSIZE / 2, YSIZE / 3),
+                [0, (Math.PI * 2) / 3, (-Math.PI * 2) / 3][dirstate[pp.id]]
+              );
+              */
+              setHash(pp, pq);
+            } else {
+              let _touch = new tapspace.Touchable(view, px, pp);
+              _touch.start({ tap: true });
+              _touch.on(
+                "tap",
+                ((px, idx,px3) => (e) => tapHandler(e, px, idx,px3))(px, idx,px3)
+              );
+            }
+            idx++;
+          });
         }
       }
     }
@@ -136,10 +211,9 @@
 
     // Make view transformable
     var tView = new tapspace.Touchable(view, view);
-    tView.start( { translate: true, rotate: true, scale: true });
+    tView.start({ translate: true, rotate: true, scale: true });
   }
   function toggleDownsideup(par) {
-    
     if (par._T.s > 0) {
       par._T.tx += 50;
       par._T.ty += 86.60254037844388;
@@ -148,51 +222,51 @@
       par._T.ty -= 86.60254037844388;
     }
     par._T.s *= -1;
-    
   }
   function isDownsideup(par) {
     let br = par._T;
-    return(Math.abs(br.r-0)<0.01 && Math.abs(br.s-(-1))<0.01);
+    return Math.abs(br.r - 0) < 0.01 && Math.abs(br.s - -1) < 0.01;
   }
-  function genKey(par,obj) {
+  function genKey(par, obj) {
     let downsideup = isDownsideup(par);
     let ar = obj._T;
     let out = [];
     let prec = 1000;
-  let pptx = 150;
-  let ppty = 86.60254037844388;
-  let xpptx = 100;
-  let xppty = 0;
-  let ret = {};
-  for(let k in ar){
-    let v = ar[k];
-    ret[k]=v;
+    let pptx = 150;
+    let ppty = 86.60254037844388;
+    let xpptx = 100;
+    let xppty = 0;
+    let ret = {};
+    for (let k in ar) {
+      let v = ar[k];
+      ret[k] = v;
+    }
+    if (downsideup) {
+      outpush(prec, out, [par._T.tx - pptx, par._T.ty - ppty]);
+    } else {
+      outpush(prec, out, [par._T.tx - xpptx, par._T.ty - xppty]);
+    }
+    return JSON.stringify([downsideup, out]);
   }
-  if (downsideup) {
-      outpush(prec,out,[par._T.tx-pptx, par._T.ty-ppty]);
-  } else {
-      outpush(prec,out,[par._T.tx-xpptx, par._T.ty-xppty]);
+  function outpush(prec, out, val) {
+    out.push([Math.round(prec * val[0]), Math.round(prec * val[1])]);
   }
-    return JSON.stringify([downsideup,out]);
-  }
-  function outpush (prec,out,val) {
-  out.push([Math.round(prec*val[0]),Math.round(prec*val[1])]);
-}
-  function checkHash(par,obj) {
-    let key = genKey(par,obj);
+  function checkHash(par, obj) {
+    let key = genKey(par, obj);
     return key in posHash;
   }
-  function deleteHash(par,obj) {
-    let key = genKey(par,obj);
+  function deleteHash(par, obj) {
+    let key = genKey(par, obj);
     delete posHash[key];
   }
-  function setHash(par,obj) {
-    let key = genKey(par,obj);
+  function setHash(par, obj) {
+    let key = genKey(par, obj);
     posHash[key] = true;
   }
   function pickRandom(arr) {
-    var i = Math.floor(Math.random() * arr.length);
-    return arr[i];
+    const rndi = Math.floor(Math.random() * arr.length);
+    const rndv = arr[rndi];
+    return { rndi, rndv };
   }
   function getCommonStyle() {
     return `
@@ -212,18 +286,37 @@
   div.tr {
   width: 100px;
   height: 100px;
-  background: rgba(68,162,225,0.5);
   transform-origin: 50px 0px;
   transform: matrix3d(1, 0, 0, 0, 0, 86.60254, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1);
   display: inline-block;
   position: absolute;
 }  
 div.white {
-  background: rgba(255,255,255,0.5);
+  background: rgba(255,255,255,1);
+}
+div.blue {
+  background: rgba(68,162,225,1);
 }
     </style>`;
   }
+
+  function getTiles() {
+    return [
+      `
+    <div style="transform-origin:50px 28.867513px;transform:scale(0.97);">
+    <div style="transform-origin:0px 0px;transform:scale(0.333)">
+    <div class="tr"></div>
+      </div></div>`,
+      `
+    <div style="transform-origin:50px 28.867513px;transform:scale(0.97);">
+    <div style="transform-origin:100px 0px;transform:scale(0.333)">
+    <div class="tr"></div>
+    </div></div>`,
+      `
+    <div style="transform-origin:50px 28.867513px;transform:scale(0.97);">
+     <div style="transform-origin:50px 86.60254px;transform:scale(0.333)">
+    <div class="tr"></div>
+    </div></div>`
+    ];
+  }
 })();
-
-
-
