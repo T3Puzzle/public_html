7da5f4de8d5d68f8a80e4f4774639eb152124ad7
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-09-20 11:14:46 +0900

    Update tileUtil.js

--- b/tileUtil.js
+++ b/tileUtil.js
@@ -1,127 +1,201 @@
 export { tileUtil };
 function tileUtil() {
+  let hasSnap = false;
   let space = null;
   let view = null;
   let grid = null;
   let root = null;
   let puz = null;
   let dupHash = {};
-  return {
-    checkFootprint,
-    wipeFootprint,
-    markFootprint,
-    init,
-    getStyle,
-    initTile,
-    placeTile,
-    final
+  // TODO : separate storage
+  let STATE = {};
+  let REST = {
+    name: "T3Puzzle",
+    title: "Simple",
+    author: "Yoshiaki Araki",
+    version: 1,
+    state: STATE
   };
-  function processGstart(gstart,e, top, tile, prevData) {
-    
-    console.log('startGend');
-    wipeFootprint(top);
-    
-    if (Number.isNaN(top._T.tx)) {
-      prevData = { tx: 0, ty: 0, r: 0, s: 1 };
+  let idHash = {};
+  let ridHash = {};
+  return {
+    init,
+    initTile,
+    placeTile
+  };
+  function detectData(top, out) {
+    let { i, j, k } = puz.detectPosition(top);
+    let id = top.id;
+    let state = getState(id);
+    if (out) {
+      out.i = i;
+      out.j = j;
+      out.k = k;
+      out.l = state.l;
+      out.m = state.m;
     } else {
-      prevData = { tx: top._T.tx, ty: top._T.ty, r: top._T.r, s: top._T.s };
+      return {
+        i: i,
+        j: j,
+        k: k,
+        l: state.l,
+        m: state.m
+      };
     }
-    /*
-    puz.loadData(top,prevData);
-    */
-    gstart(e, top, tile);
   }
-  function processGend(gend, e, top, tile, prevData) {
-    if (detailGend(top,tile,prevData)){
+  function setPartialState(id, state) {
+    if (id in STATE) {
+      STATE[id].l = state.l;
+      STATE[id].m = state.m;
+    } else if (id in ridHash) {
+      STATE[ridHash[id]].l = state.l;
+      STATE[ridHash[id]].m = state.m;
+    } else {
+      //
+      console.log("never happen:set:" + id);
+    }
+  }
+  function getState(id) {
+    if (id in STATE) {
+      return STATE[id];
+    } else if (id in ridHash) {
+      return STATE[ridHash[id]];
+    } else {
+      //
+      console.log("never happen:get:" + id);
+    }
+  }
+  function shareState(id, data, oldData, local) {
+    markFootprint(data);
+    if (JSON.stringify(data) == JSON.stringify(oldData)) {
       return;
     }
-    markFootprint(top, tile);
-    gend(e, top, tile);
-  }
-  function detailGend(top,tile,prevData) {
-    console.log('detailGend');
-    let dx = -top._T.tx;
-    let dy = -top._T.ty;
-    top.snap(tile.atMid(), grid);
-    
-    dx += top._T.tx;
-    dy += top._T.ty;
-    let norm = Math.sqrt(dx * dx + dy * dy);
-    console.log("norm:" + norm + " " + top._T.tx + " " + top._T.ty);
-
-    if (!Number.isNaN(norm)) {
-      if (norm > puz.XSIZE / 2) {
-        console.log("flipping");
-        puz.toggleDownsideup(top, tile);
-        top.snap(tile.atMid(), grid);
-        // rotate after snap ....
-        puz.adjustTurn(top, tile);
-      }
-      if (!checkFootprint(top)) {
-        console.log("safe" + puz.getFootprint(top));
-        return false;
-      }
+    let state = JSON.parse(JSON.stringify(data));
+    if (id in STATE) {
+      STATE[id] = state;
+    } else if (id in ridHash) {
+      STATE[ridHash[id]] = state;
     } else {
-      // workaround
-      let topNode = view.getElementBySpaceItem(top); 
-      topNode.remove();
-      puz.getInitTileCallback()({i:-2,j:-2,k:0,l:0,m:0});
-      return true;
+      if (local) {
+        STATE[id] = state;
+      } else {
+        //call just after recreated before renameId
+        //console.log('never happen:sharer:'+id);
+      }
     }
-    console.log("dup" + puz.getFootprint(top));
-    top._T.tx = prevData.tx;
-    top._T.ty = prevData.ty;
-    top._T.r = prevData.r;
-    top._T.s = prevData.s;
-    top.snap(tile.atMid(), grid);
-    return false;
-    /*
-    placeTile(top,tile,prevData);
-    */
+    if (!local) {
+      localStorage.setItem("STATE", JSON.stringify(REST));
+      //localStorage.setItem('STATE',JSON.stringify(STATE));
+      //localStorage.setItem('DUP',JSON.stringify(dupHash));
+    }
   }
-  function placeTile(top, tile, data, nodup) {
+  function renameId(fromId, toId) {
+    let orgId = null;
+    if (fromId in ridHash) {
+      orgId = ridHash[fromId];
+      //why not?
+      //delete ridHash[fromId];
+    } else {
+      orgId = fromId;
+    }
+    idHash[orgId] = toId;
+    ridHash[toId] = orgId;
+  }
+  function processGstart(gstart, e, target, prevData) {
+    let top = target.top;
+    let tile = target.tile;
+    top.bringToFront();
+    detectData(top, prevData);
+    wipeFootprint(prevData);
+    gstart(e, target);
+  }
+  function processGend(gend, e, target, prevData) {
+    let top = target.top;
+    let tile = target.tile;
+    detailGend(target, prevData);
+    gend(e, target);
+    top = target.top;
     //
-    //console.log();
-    top.translate(tile.atMid(), grid.at(data.i, data.j));
-    puz.adjustTransform(data.k, top, tile);
-    top.snap(tile.atMid(), grid);
-    // rotate after snap ....
-    //tile.rotate(tile.at(puz.XCENTER, puz.YCENTER), puz.ROTATES[data.l]);
-    puz.setFace(top, tile, data);
-    // huummm. snap changes data...
-    puz.storeState(top, data);
-    if (!nodup) {
-      markFootprint(top, tile);
+    shareState(top.id, detectData(top), prevData);
+    //
+  }
+  function processTap(thand, e, target, idx) {
+    let top = target.top;
+    let oldData = detectData(top);
+    thand(e, target, idx);
+    top = target.top;
+    //
+    shareState(top.id, detectData(top), oldData);
+    //
+  }
+  function detailGend(target, prevData) {
+    let top = target.top;
+    let tile = target.tile;
+    let data = detectData(top);
+    if (!checkFootprint(data)) {
+      //console.log("SAFE=>" + data);
+      let fromId = top.id;
+      data = detectData(top);
+      recreateTarget(target, data);
+      puz.adjustTurn(target, data, prevData);
+    } else {
+      //console.log("DUP =>" +data);
+      recreateTarget(target, prevData);
+      data = detectData(top);
     }
   }
-  
-  function initTile(data, tapHandler, gstart, gend, gmid) {
+  function recreateTarget(target, data) {
+    let top = target.top;
+    let fromId = top.id;
+    let topNode = view.getElementBySpaceItem(top);
+    topNode.remove();
+    let _target = puz.getInitTileCallback()(data, { local: false });
+    target.top = _target.top;
+    target.tile = _target.tile;
+    top = target.top;
+    let toId = top.id;
+    renameId(fromId, toId);
+  }
+  function placeTile(target, data) {
+    let top = target.top;
+    let tile = target.tile;
+    top.translate(tile.atMid(), grid.at(data.i, data.j));
+    puz.adjustTransform(data, target);
+    top.snap(tile.atMid(), grid);
+    puz.setFace(target, data);
+  }
+  function initTile(data, thand, gstart, gend, gmid, opt) {
     let top = new tapspace.SpaceGroup(root);
+    // share local
+    shareState(top.id, data, {}, opt.local);
+
     let tileHtml = puz.getTileHtml();
     let tile = new tapspace.SpaceHTML(tileHtml, top).setSize(0, 0);
     // place tile here
-    gmid(top, tile, data);
-    // TODO : data => pos ==snap==> data
-    // prev
-    let prevData = { tx: top._T.tx, ty: top._T.ty, r: top._T.r, s: top._T.s };
-    /*let prevData = {};
-    puz.loadData(top,prevData);
-    */
+    let target = { top, tile };
+    gmid(target, data);
+
+    ////
+    let prevData = {};
     // gesture
     let gesture = new tapspace.Touchable(view, top);
     gesture.start({ translate: true });
-    gesture.on("gesturestart", (e) => processGstart(gstart,e, top, tile, prevData));
-    gesture.on("gestureend", (e) => processGend(gend, e, top, tile, prevData));
+    gesture.on("gesturestart", (e) =>
+      processGstart(gstart, e, target, prevData)
+    );
+    gesture.on("gestureend", (e) => processGend(gend, e, target, prevData));
     // tap
     puz.getTapPanels().forEach((panelHtml, idx) => {
       let panel = new tapspace.SpaceHTML(panelHtml, top).setSize(0, 0);
       // tap panel
       let tap = new tapspace.Touchable(view, panel);
-      tap.start({ tap: true});
-      tap.on("tap", (e) => tapHandler(e, top, tile, idx));
+      tap.start({ tap: true });
+      tap.on("tap", (e) => processTap(thand, e, target, idx));
     });
+
+    return target;
   }
-  function init(custom, _puz) {
+  function init(custom, _puz, callback) {
     puz = _puz;
     let shadow = custom.attachShadow({ mode: "open" });
     // style
@@ -151,7 +225,9 @@ function tileUtil() {
       space
     );
     root = new tapspace.SpaceGroup(space);
-    puz.init(view);
+    puz.init(view, { set: setPartialState, get: getState });
+    callback();
+    final();
   }
   function final() {
     view.fitScale(root);
@@ -159,16 +235,19 @@ function tileUtil() {
     var tView = new tapspace.Touchable(view, view);
     tView.start({ translate: true, rotate: true, scale: true });
   }
-  function checkFootprint(top) {
-    let fp = puz.getFootprint(top);
+  function checkFootprint(data) {
+    let fp = JSON.stringify([data.i, data.j, data.k]);
+    //console.log("check:" + fp);
     return fp in dupHash;
   }
-  function wipeFootprint(top) {
-    let fp = puz.getFootprint(top);
+  function wipeFootprint(data) {
+    let fp = JSON.stringify([data.i, data.j, data.k]);
+    //console.log("wipe:" + fp);
     delete dupHash[fp];
   }
-  function markFootprint(top) {
-    let fp = puz.getFootprint(top);
+  function markFootprint(data) {
+    let fp = JSON.stringify([data.i, data.j, data.k]);
+    //console.log("mark:" + fp);
     dupHash[fp] = true;
   }
   function getStyle() {
