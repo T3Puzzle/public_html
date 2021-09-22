f57097f712bdd19ec743b05e09c532a2b6fbff10
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-09-23 07:26:55 +0900

    Update tileUtil.js

--- b/tileUtil.js
+++ b/tileUtil.js
@@ -1,13 +1,26 @@
 export { tileUtil };
 function tileUtil() {
+  let me = null;
   let hasSnap = false;
   let space = null;
   let view = null;
   let grid = null;
   let root = null;
+  let board = null;
   let puz = null;
   let dupHash = {};
-  // TODO : separate storage
+  let READONLY = {
+    tiles: [],
+    enabled: false
+  };
+  let ANCHOR = {
+    ground: null,
+    enabled: false
+  };
+  let HAND = {
+    ground: null,
+    enabled: false
+  };
   let STATE = {};
   let REST = {
     name: "T3Puzzle",
@@ -16,13 +29,101 @@ function tileUtil() {
     version: 1,
     state: STATE
   };
+  let targetHash = {};
   let idHash = {};
   let ridHash = {};
+  const VERSION = 1.0;
   return {
     init,
     initTile,
-    placeTile
+    placeTile,
+    changeMode,
+    flipAll,
+    VERSION
   };
+  function shareState(target, id, data, oldData, local) {
+    markFootprint(data);
+    if (JSON.stringify(data) == JSON.stringify(oldData)) {
+      return;
+    }
+    let state = JSON.parse(JSON.stringify(data));
+    if (id in STATE) {
+      STATE[id] = state;
+    } else if (id in ridHash) {
+      STATE[ridHash[id]] = state;
+    } else {
+      if (local) {
+        STATE[id] = state;
+        targetHash[id] = target;
+      } else {
+        //call just after recreated before renameId
+        //console.log('never happen:sharer:'+id);
+      }
+    }
+    if (!local) {
+      shareStateAll();
+    }
+  }
+  function shareStateAll() {
+    let value = { detail: { state: STATE } };
+    me.value = value;
+    me.dispatchEvent(new CustomEvent("change", value));
+  }
+  function processGstart(gstart, e, target, prevData) {
+    let top = target.top;
+    let imgback = target.imgback;
+    imgback.bringToFront();
+    top.bringToFront();
+    detectData(top, prevData);
+    wipeFootprint(prevData);
+    gstart(e, target);
+  }
+  function processGmove(gmove, e, target, prevData) {
+    let top = view.getElementBySpaceItem(target.top);
+    let imgback = view.getElementBySpaceItem(target.imgback);
+    imgback.style.cssText = top.style.cssText;
+  }
+  function processGend(gend, e, target, prevData) {
+    let top = target.top;
+    let imgback = target.imgback;
+    imgback.sendToBack();
+    if (HAND.enabled) {
+      deleteTarget(target);
+      shareStateAll();
+    } else {
+      detailGend(target, prevData);
+      gend(e, target);
+      top = target.top;
+    //
+      shareState(target, top.id, detectData(top), prevData);
+    }
+    //
+  }
+  function processTap(thand, e, target, idx) {
+    let top = target.top;
+    let oldData = detectData(top);
+    thand(e, target, idx);
+    top = target.top;
+    //
+    shareState(target, top.id, detectData(top), oldData);
+    //
+  }
+  function detailGend(target, prevData) {
+    let top = target.top;
+    let tile = target.tile;
+    let data = detectData(top);
+    if (!checkFootprint(data)) {
+      //console.log("SAFE=>" + data);
+      let fromId = top.id;
+      data = detectData(top);
+      recreateTarget(target, data);
+      //?????
+      puz.adjustTurn(target, data, prevData);
+    } else {
+      //console.log("DUP =>" +data);
+      recreateTarget(target, prevData);
+    }
+  }
   function detectData(top, out) {
     let { i, j, k } = puz.detectPosition(top);
     let id = top.id;
@@ -33,23 +134,55 @@ function tileUtil() {
       out.k = k;
       out.l = state.l;
       out.m = state.m;
+      out.n = state.n;
     } else {
       return {
         i: i,
         j: j,
         k: k,
         l: state.l,
-        m: state.m
+        m: state.m,
+        n: state.n
       };
     }
+  }
+  function renameId(fromId, target) {
+    let orgId = fromId;
+    if (fromId in ridHash) {
+      orgId = ridHash[fromId];
+    }
+    let toId = target.top.id;
+    targetHash[orgId] = target;
+    idHash[orgId] = toId;
+    ridHash[toId] = orgId;
+    if (fromId in ridHash) {
+      //why not?
+      delete ridHash[fromId];
+    }
+  }
+  function deleteIdAndSo(id) {
+    let orgId = id;
+    if (id in ridHash) {
+      orgId = ridHash[id];
+    }
+    delete targetHash[orgId];
+    delete idHash[orgId];
+    if (id in ridHash) {
+      delete ridHash[id];
+    }
+    if (id in STATE) {
+      delete STATE[id];
+    } 
   }
   function setPartialState(id, state) {
     if (id in STATE) {
       STATE[id].l = state.l;
       STATE[id].m = state.m;
+      STATE[id].n = state.n;
     } else if (id in ridHash) {
       STATE[ridHash[id]].l = state.l;
       STATE[ridHash[id]].m = state.m;
+      STATE[ridHash[id]].n = state.n;
     } else {
       //
       console.log("never happen:set:" + id);
@@ -65,153 +198,103 @@ function tileUtil() {
       console.log("never happen:get:" + id);
     }
   }
-  function shareState(id, data, oldData, local) {
-    markFootprint(data);
-    if (JSON.stringify(data) == JSON.stringify(oldData)) {
-      return;
-    }
-    let state = JSON.parse(JSON.stringify(data));
-    if (id in STATE) {
-      STATE[id] = state;
-    } else if (id in ridHash) {
-      STATE[ridHash[id]] = state;
-    } else {
-      if (local) {
-        STATE[id] = state;
-      } else {
-        //call just after recreated before renameId
-        //console.log('never happen:sharer:'+id);
-      }
-    }
-    if (!local) {
-      localStorage.setItem("STATE", JSON.stringify(REST));
-      //localStorage.setItem('STATE',JSON.stringify(STATE));
-      //localStorage.setItem('DUP',JSON.stringify(dupHash));
-    }
-  }
-  function renameId(fromId, toId) {
-    let orgId = null;
-    if (fromId in ridHash) {
-      orgId = ridHash[fromId];
-      //why not?
-      //delete ridHash[fromId];
-    } else {
-      orgId = fromId;
-    }
-    idHash[orgId] = toId;
-    ridHash[toId] = orgId;
-  }
-  function processGstart(gstart, e, target, prevData) {
+  function deleteTarget(target) {
     let top = target.top;
-    let tile = target.tile;
-    top.bringToFront();
-    detectData(top, prevData);
-    wipeFootprint(prevData);
-    gstart(e, target);
-  }
-  function processGend(gend, e, target, prevData) {
-    let top = target.top;
-    let tile = target.tile;
-    detailGend(target, prevData);
-    gend(e, target);
-    top = target.top;
-    //
-    shareState(top.id, detectData(top), prevData);
-    //
-  }
-  function processTap(thand, e, target, idx) {
-    let top = target.top;
-    let oldData = detectData(top);
-    thand(e, target, idx);
-    top = target.top;
-    //
-    shareState(top.id, detectData(top), oldData);
-    //
-  }
-  function detailGend(target, prevData) {
-    let top = target.top;
-    let tile = target.tile;
-    let data = detectData(top);
-    if (!checkFootprint(data)) {
-      //console.log("SAFE=>" + data);
-      let fromId = top.id;
-      data = detectData(top);
-      recreateTarget(target, data);
-      puz.adjustTurn(target, data, prevData);
-    } else {
-      //console.log("DUP =>" +data);
-      recreateTarget(target, prevData);
-      data = detectData(top);
-    }
+    let id = top.id;
+    deleteIdAndSo(id);
+    //let topNode = view.getElementBySpaceItem(top);
+    //topNode.remove();
+    top.remove();
+    target.imgback.remove();
   }
   function recreateTarget(target, data) {
     let top = target.top;
     let fromId = top.id;
-    let topNode = view.getElementBySpaceItem(top);
-    topNode.remove();
+    //let topNode = view.getElementBySpaceItem(top);
+    //topNode.remove();
+    top.remove();
+    target.imgback.remove();
     let _target = puz.getInitTileCallback()(data, { local: false });
     target.top = _target.top;
     target.tile = _target.tile;
-    top = target.top;
-    let toId = top.id;
-    renameId(fromId, toId);
+    target.imgback = _target.imgback;
+    target.img0 = _target.img0;
+    renameId(fromId, target);
   }
   function placeTile(target, data) {
     let top = target.top;
     let tile = target.tile;
+    let imgback = target.imgback;
+    let img0 = target.img0;
     top.translate(tile.atMid(), grid.at(data.i, data.j));
+    imgback.translate(img0.atMid(), grid.at(data.i, data.j));
     puz.adjustTransform(data, target);
     top.snap(tile.atMid(), grid);
     puz.setFace(target, data);
+    
   }
   function initTile(data, thand, gstart, gend, gmid, opt) {
+    
+    let imgback = new tapspace.SpaceGroup(root);
+    imgback.sendToBack();
+    let img0 = new tapspace.SpaceHTML(puz.getImgHtml(), imgback).setSize(0, 0);
+    
     let top = new tapspace.SpaceGroup(root);
-    // share local
-    shareState(top.id, data, {}, opt.local);
 
     let tileHtml = puz.getTileHtml();
     let tile = new tapspace.SpaceHTML(tileHtml, top).setSize(0, 0);
     // place tile here
-    let target = { top, tile };
+    let target = { top, tile, imgback, img0 };
+    // share local
     gmid(target, data);
-
+    addCursor();
+    shareState(target, top.id, data, {}, opt.local);
     ////
     let prevData = {};
     // gesture
     let gesture = new tapspace.Touchable(view, top);
-    gesture.start({ translate: true });
+    READONLY.tiles.push(gesture);
+      gesture.start({ translate: true });
     gesture.on("gesturestart", (e) =>
-      processGstart(gstart, e, target, prevData)
-    );
+      processGstart(gstart, e, target, prevData));
+    gesture.on("gesturemove",(e)=>
+              processGmove(null,e,target,prevData));
     gesture.on("gestureend", (e) => processGend(gend, e, target, prevData));
+    if (READONLY.enabled) {
+      gesture.stop();
+    }
     // tap
     puz.getTapPanels().forEach((panelHtml, idx) => {
       let panel = new tapspace.SpaceHTML(panelHtml, top).setSize(0, 0);
       // tap panel
       let tap = new tapspace.Touchable(view, panel);
+      READONLY.tiles.push(tap);
       tap.start({ tap: true });
       tap.on("tap", (e) => processTap(thand, e, target, idx));
+      if (READONLY.enabled) {
+        tap.stop();
+      }
     });
-
     return target;
   }
-  function init(custom, _puz, callback) {
+  function init(_me, _puz, callback) {
+    me = _me;
     puz = _puz;
-    let shadow = custom.attachShadow({ mode: "open" });
+    me.attachShadow({ mode: "open" });
     // style
     let style = document.createElement("style");
-    style.innerHTML = puz.getCSS();
-    shadow.append(style);
+    me.shadowRoot.append(style);
+    style.innerHTML = puz.getCSS()+getCSS();
     // mount point
     let div = document.createElement("div");
     div.style = getStyle();
-    shadow.append(div);
-
+    me.shadowRoot.append(div);
     let commonStyle = getStyle();
     document.querySelector("html").style = commonStyle;
     document.querySelector("body").style = commonStyle;
     space = new tapspace.Space();
     view = new tapspace.SpaceView(space);
+    
     view.mount(div);
     grid = new tapspace.geom.IGrid(
       {
@@ -225,6 +308,9 @@ function tileUtil() {
       space
     );
     root = new tapspace.SpaceGroup(space);
+    
+    board = new tapspace.SpaceHTML('<div style="width:5000px;height:5000px;transform:translate(-2500px,-2500px)"></div>', root).setSize(0, 0);
+    
     puz.init(view, { set: setPartialState, get: getState });
     callback();
     final();
@@ -232,8 +318,187 @@ function tileUtil() {
   function final() {
     view.fitScale(root);
     view.scale(view.atMid(), 1.618);
-    var tView = new tapspace.Touchable(view, view);
-    tView.start({ translate: true, rotate: true, scale: true });
+
+    let touch = new tapspace.Touchable(view, view);
+    touch.start({ translate: true, rotate: true, scale: true });
+    ANCHOR.ground = touch;
+    HAND.ground = touch;
+    if (ANCHOR.enabled) {
+      changeMode(null,'anchor');
+      ANCHOR.ground.stop();
+    } else if (HAND.enabled) {
+      changeMode(null,'hand');
+      HAND.ground.stop();
+      startBaseClick();
+    } else if (READONLY.enabled) {
+      changeMode(null,'readonly');
+    } else {
+      changeMode(null,null);
+    }
+  }
+  function addCursor() {
+    let spaceNode = view.getElementBySpaceItem(space);
+    if (ANCHOR.enabled) {
+      Array.from(spaceNode.querySelectorAll('div.tr.tr__base')).map(t=>{
+        t.classList.add('cursor--grab');
+      });
+    } else if (READONLY.enabled) {
+      // nop
+    } else if (HAND.enabled) {
+      Array.from(spaceNode.querySelectorAll('div.tr.tr__base')).map(t=>{
+        t.classList.add('cursor--nodrop');
+      });
+    } else {
+      Array.from(spaceNode.querySelectorAll('div.tr.tr__base')).map(t=>{
+        t.classList.add('cursor--grab');
+      });
+    }
+  }
+  function changeMode (oldValue,newValue) {
+    if (!space){
+      return;
+    }
+    let spaceNode = view.getElementBySpaceItem(space);
+    let baseNode = view.getElementBySpaceItem(board);
+    if (oldValue==='anchor') {
+      enableAnchor(false);
+      baseNode.classList.remove('cursor--default');
+      spaceNode.classList.remove('cursor--pointer');
+      Array.from(spaceNode.querySelectorAll('div.tr.tr__base')).map(t=>{
+        t.classList.remove('cursor--grab');
+      });
+    } else if (oldValue==='readonly') {
+      enableReadonly(false);
+      spaceNode.classList.remove('cursor--move');
+    } else if (oldValue==='hand') {
+      enableHand(false);
+      baseNode.classList.remove('cursor--copy');
+      spaceNode.classList.remove('cursor--pointer');
+      Array.from(spaceNode.querySelectorAll('div.tr.tr__base')).map(t=>{
+        t.classList.remove('cursor--nodrop');
+      });
+    } else {
+      baseNode.classList.remove('cursor--move');
+      spaceNode.classList.remove('cursor--pointer');
+      Array.from(spaceNode.querySelectorAll('div.tr.tr__base')).map(t=>{
+        t.classList.remove('cursor--grab');
+      });
+    }
+    ////
+    if (newValue==='anchor') {
+      enableAnchor(true);
+      baseNode.classList.add('cursor--default');
+      spaceNode.classList.add('cursor--pointer');
+      addCursor();
+    } else if (newValue==='readonly') {
+      enableReadonly(true);
+      spaceNode.classList.add('cursor--move');
+      addCursor();
+    } else if (newValue==='hand') {
+      enableHand(true);
+      baseNode.classList.add('cursor--copy');
+      spaceNode.classList.add('cursor--pointer');
+      addCursor();
+    } else {
+      baseNode.classList.add('cursor--move');
+      spaceNode.classList.add('cursor--pointer');
+      addCursor();
+    }
+  }
+  function enableReadonly(newValue) {
+    let oldValue = READONLY.enabled;
+    if (oldValue === newValue) {
+      // nop
+      return;
+    }
+    READONLY.enabled = newValue;
+    if (READONLY.enabled) {
+      if (READONLY.tiles) {
+        READONLY.tiles.map((t) => {
+          t.stop();
+        });
+      }
+    } else {
+      READONLY.tiles.map((t) => {
+        t.resume();
+      });
+    }
+  }
+  function enableAnchor(newValue) {
+    let oldValue = ANCHOR.enabled;
+    if (oldValue === newValue) {
+      // nop
+      return;
+    }
+    ANCHOR.enabled = newValue;
+    if (ANCHOR.enabled) {
+      if (ANCHOR.ground) {
+        ANCHOR.ground.stop();
+      }
+    } else {
+      ANCHOR.ground.resume();
+    }
+  }
+  function enableHand(newValue) {
+    let oldValue = HAND.enabled;
+    if (oldValue === newValue) {
+      // nop
+      return;
+    }
+    HAND.enabled = newValue;
+    if (HAND.enabled) {
+      if (HAND.ground) {
+        HAND.ground.stop();
+        startBaseClick();
+      }
+    } else {
+      stopBaseClick();
+      HAND.ground.resume();
+    }
+  }
+  function startBaseClick() {
+    //let baseNode = view.getElementBySpaceItem(space).parentNode;
+    let baseNode = view.getElementBySpaceItem(board);
+    baseNode.addEventListener("click", baseClick);
+  }
+  function stopBaseClick() {
+    //let baseNode = view.getElementBySpaceItem(space).parentNode;
+    let baseNode = view.getElementBySpaceItem(board);
+    baseNode.removeEventListener("click", baseClick);
+  }
+  function baseClick(e) {
+    let spaceNode = view.getElementBySpaceItem(space);
+    let transformStr = spaceNode.style.getPropertyValue("transform");
+    let matrix = (a, b, c, d, e, f) => {
+      return { c: a, s: b, tx: e, ty: f };
+    };
+    let m = eval("(()=>" + transformStr + ")();");
+    let scale = Math.sqrt(m.c*m.c+m.s*m.s);
+    let theta = Math.atan2(m.s,m.c);
+    let cos = Math.cos(-theta);
+    let sin = Math.sin(-theta);
+    let sx = (e.clientX - m.tx)/scale;
+    let sy = (e.clientY - m.ty)/scale;
+    let dx = cos * sx - sin * sy;
+    let dy = sin * sx + cos * sy; 
+    let data = puz.detectPoint(dx,dy);
+    if (checkFootprint(data) ) {
+      // nop
+      return;
+    }
+    let pos = { x: dx , y: dy };
+    let target = puz.getInitTileCallback()(data, { local: false });
+    let id = target.top.id;
+    STATE[id] = data;
+    targetHash[id] = target;
+    shareStateAll();
+  }
+  function flipAll() {
+    for (let id in STATE) {
+      STATE[id].m = (STATE[id].m + 1) % 2;
+      puz.setFace(targetHash[id], STATE[id]);
+    }
+    shareStateAll();
   }
   function checkFootprint(data) {
     let fp = JSON.stringify([data.i, data.j, data.k]);
@@ -249,6 +514,27 @@ function tileUtil() {
     let fp = JSON.stringify([data.i, data.j, data.k]);
     //console.log("mark:" + fp);
     dupHash[fp] = true;
+  }
+  function getCSS() {
+    return `
+    div.cursor--default {
+      cursor: default;
+    }
+    div.cursor--move {
+      cursor: move;
+    }
+    div.cursor--grab{
+      cursor: grab;
+    }
+    div.cursor--copy {
+      cursor: copy;
+    }
+    div.cursor--pointer {
+      cursor: pointer;
+    }
+    div.cursor--nodrop {
+      cursor: no-drop;
+    }`; 
   }
   function getStyle() {
     return `
