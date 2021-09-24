0d19b3ce0ab367d89900fb2d696349a297b7bdb9
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-09-24 20:51:32 +0900

    Create t3.js

---
+++ a/t3.js
@@ -0,0 +1,84 @@
+importModules(
+  "tileUtil",
+  "./tileUtil.js",
+  "https://codepen.io/alytile/pen/dyRZgga.js",
+  (tileUtil) => {
+    importModules(
+      "t3Util",
+      "./t3Util.js",
+      "https://codepen.io/alytile/pen/xxrPQob.js",
+      (t3Util) => {
+        define(tileUtil, t3Util);
+      }
+    );
+  }
+);
+function define(util, puz) {
+  customElements.define(
+    "grid-puzzle",
+    class extends HTMLElement {
+      static get observedAttributes() {
+        return ["color", "mode", "flipall","fit","loaddata"];
+      }
+      attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "color") {
+          puz.setNowColor(newValue);
+        } else if (name === "mode") {util.changeMode(oldValue,newValue);
+        } else if (name === "flipall") {
+          util.flipAll();
+        } else if (name === "fit") {
+          util.fit();
+        } else if (name === "loaddata") {
+          util.resetAndloadData(newValue);
+        }
+      }
+      connectedCallback() {
+        util.init(this, puz, process);
+      }
+    }
+  );
+  function process() {
+    // customize initial position
+    util.load({}, (data, opt) =>
+      util.initTile(
+        data,
+        tapHandler,
+        gstart,
+        gend,
+        (target, data) => {
+          util.placeTile(target, data);
+        },
+        opt
+      )
+    );
+  }
+  function gstart(ev, target) {
+    // customzie on gesturestart event
+  }
+  function gend(ev, target) {
+    // customize gestureend event
+  }
+  function tapHandler(ev, target, idx) {
+    // customize effect on tap panels.
+    
+    puz.switchFace(target, idx);
+  }
+}
+function importModules(moduleName, first, second, callback) {
+  let ret = null;
+  import(first)
+    .then((module) => {
+      ret = new module[moduleName]();
+      callback(ret);
+    })
+    .catch((e) => {
+      import(second)
+        .then((module) => {
+          ret = new module[moduleName]();
+          callback(ret);
+        })
+        .catch((e) => {
+          console.log("error");
+        });
+    });
+}
