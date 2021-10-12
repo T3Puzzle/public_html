da45bd0cf376ed7dcc4ae4379fe1b97452075d00
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-10-12 14:02:28 +0900

    Add files via upload

--- a/automata.js
+++ a/automata.js
@@ -97,7 +97,7 @@ function next ( me ) {
   let abc = [a.l,b.l,c.l,a.m,b.m,c.m];
   let key = JSON.stringify(abc);
   let val = dh[key];
-if ( !('0' in val )) {
+if ( !val )) {
   console.log(key);
 }
   return updateMe(me, val[0], val[1]);
