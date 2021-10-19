b175b81d55cab61befa7ee177dd784c2d2df4e21
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-10-19 09:38:26 +0900

    Update buttonText.js

--- n/buttonText.js
+++ n/buttonText.js
@@ -1,3 +1,4 @@
+
 (() => {
   customElements.define(
     "button-text",
@@ -5,105 +6,52 @@
       constructor() {
         super();
         this.__SIZE = "100px";
-        this.__COLOR = "rgb(180,0,0)";
-        this.__CHECKED = false;
-        this.__DISABLED = false;
         this.attachShadow({ mode: "open" });
       }
       connectedCallback() {
+        let me = this;
         let style = document.createElement("style");
         style.innerHTML = this.__getCSS();
         this.shadowRoot.append(style);
         let a = document.createElement("a");
         a.href = "#";
         a.classList.add("button");
-        a.addEventListener("click", () => {
-          this.__toggleButton(a);
-        });
         this.shadowRoot.append(a);
         let block = document.createElement("div");
         block.classList.add("button");
         a.append(block);
         let text = this.getAttribute("text");
+        let divs = [];
         Array.from(text).map((t) => {
           let div = document.createElement("div");
+          divs.push(div);
           div.classList.add("button__text");
           div.innerText = t;
           block.append(div);
         });
-        this.__disable(this.__DISABLED);
-        this.__check(this.__CHECKED);
+        a.addEventListener("mousedown", () => {
+          divs.map(d=>d.classList.add('button__text--shadow'));
+        });
+        a.addEventListener("mouseup", () => {
+          divs.map(d=>d.classList.remove('button__text--shadow'));
+        });
       }
       static get observedAttributes() {
-        return ["size", "disabled", "checked", "color"];
+        return ["size"];
       }
       attributeChangedCallback(name, oldValue, newValue) {
         if (name === "size") {
           this.__setSize(newValue);
-        } else if (name === "color") {
-          this.__setColor(newValue);
-        } else if (name === "disabled") {
-          this.__DISABLED = !(newValue===null);
-          this.__disable(this.__DISABLED);
-        } else if (name === "checked") {
-          this.__CHECKED = !(newValue===null);
-          this.__check(this.__CHECKED);
         }
-      }
-      __check(value) {
-        if (this.__DISABLED) {
-          return;
-        }
-        this.__CHECKED = value;
-        let texts = this.shadowRoot.querySelectorAll("div.button__text");
-        Array.from(texts).map((text) => {
-          if (this.__CHECKED) {
-            text.classList.add("button__text--checked");
-          } else {
-            text.classList.remove("button__text--checked");
-          }
-        });
-      }
-      __disable(value) {
-        this.__DISABLED = value;
-        let texts = this.shadowRoot.querySelectorAll("div.button__text");
-        Array.from(texts).map((text) => {
-          if (this.__DISABLED) {
-            text.classList.add("button__text--disabled");
-          } else {
-            text.classList.remove("button__text--disabled");
-          }
-        });
-      }
-      __setColor(value) {
-        // TODO: to be checked.
-        this.__COLOR = value;
       }
       __setSize(value) {
         // TODO: to be checked.
         this.__SIZE = value;
       }
-      __toggleButton(target) {
-        if (this.__DISABLED) {
-          return;
-        }
-        this.__CHECKED = !this.__CHECKED;
-        if (this.__CHECKED) {
-          this.setAttribute('checked','checked');
-        } else {
-          this.removeAttribute('checked');
-        }
-        let value = { detail: { checked: this.__CHECKED } };
-        this.value = this.__CHECKED;
-        this.dispatchEvent(new CustomEvent("change", value));
-      }
       __getCSS() {
         return `
 a.button {
   display: inline-block;
-}
-div.button__text--disabled {
-  opacity: 0.2;
 }
 div.button {
   width: ${this.__SIZE};
@@ -117,10 +65,10 @@ div.button__text {
   font-size: ${this.__SIZE};  
   color: rgb(0,0,0);
   font-family: Gill Sans Extrabold, sans-serif;
-  color:transparent; text-shadow: 0 0 0 rgb(90,90,90);
+  text-shadow: 1px 1px 1px #808080;
 }
-div.button__text--checked {
-  color:transparent; text-shadow: 0 0 0 ${this.__COLOR};
+div.button__text--shadow {
+  text-shadow: 3px 6px 9px #808080;
 }
 `;
       }
