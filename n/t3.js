1bb6f98c705cfcd2754fb378e9c78144442cb126
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-10-19 09:33:03 +0900

    Update t3.js

--- n/t3.js
+++ n/t3.js
@@ -1,81 +1,70 @@
-importModules(
-  "tileUtil",
-  "./tileUtil.js",
-  "https://codepen.io/alytile/pen/dyRZgga.js",
-  (tileUtil) => {
-    importModules(
-      "t3Util",
-      "./t3Util.js",
-      "https://codepen.io/alytile/pen/xxrPQob.js",
-      (t3Util) => {
-        define(tileUtil, t3Util);
-      }
-    );
-  }
-);
+import { tapspace } from 'https://www.t3puzzle.com/n/tapspace.module.min.js?zo';
+export {t3};
+function t3 () {
+  return { define: define };
+}
 function define(util, puz) {
   customElements.define(
-    "tapspace-grid",
+    "grid-puzzle",
     class extends HTMLElement {
       static get observedAttributes() {
-        return ["color", "mode", "flipall"];
+        return [
+          "color",
+          "mode",
+          "flipall",
+          "fit",
+          "rotate",
+          "zoom",
+          "loaddata",
+          "nomoveall",
+          "noflipall"
+        ];
       }
       attributeChangedCallback(name, oldValue, newValue) {
         if (name === "color") {
-          puz.setTileColor(newValue);
+          this.puz.setNowColor(newValue);
         } else if (name === "mode") {
-          util.changeMode(oldValue,newValue);
+          this.util.changeMode(oldValue, newValue);
+          this.mode = newValue;
         } else if (name === "flipall") {
-          util.flipAll();
+          this.util.flipAll();
+        } else if (name === "fit") {
+          this.util.fit();
+        } else if (name === "rotate") {
+          this.util.rotate(newValue);
+         } else if (name === "zoom") {
+          this.util.zoom(newValue);
+        } else if (name === "loaddata") {
+          this.util.resetAndloadData(newValue);
+        } else if (name === "nomoveall") {
+          this.util.noMoveAll(newValue);
+        } else if (name === "noflipall") {
+          this.util.noFlipAll(newValue);
         }
       }
-      connectedCallback() {
-        util.init(this, puz, process);
+      constructor() {
+        super();
+        this.util = util();
+        this.puz = puz();
+        this.mode = "";/*
+      }
+      connectedCallback() {*/
+        this.util.init(this, this.puz, () => {
+          // customize initial position
+          return this.util.load({}, (data, opt) =>
+            this.util.initTile(
+              data,
+              (ev, target, idx) => this.puz.switchFace(target, idx),
+              (ev, target) => {},
+              (ev, target) => {},
+              (target, data) => {
+                this.util.placeTile(target, data);
+              },
+              opt
+            )
+          );
+        });
       }
     }
   );
-  function process() {
-    // customize initial position
-    puz.load({}, (data, opt) =>
-      util.initTile(
-        data,
-        tapHandler,
-        gstart,
-        gend,
-        (target, data) => {
-          util.placeTile(target, data);
-        },
-        opt
-      )
-    );
-  }
-  function gstart(ev, target) {
-    // customzie on gesturestart event
-  }
-  function gend(ev, target) {
-    // customize gestureend event
-  }
-  function tapHandler(ev, target, idx) {
-    // customize effect on tap panels.
-    
-    puz.switchFace(target, idx);
-  }
-}
-function importModules(moduleName, first, second, callback) {
-  let ret = null;
-  import(first)
-    .then((module) => {
-      ret = new module[moduleName]();
-      callback(ret);
-    })
-    .catch((e) => {
-      import(second)
-        .then((module) => {
-          ret = new module[moduleName]();
-          callback(ret);
-        })
-        .catch((e) => {
-          console.log("error");
-        });
-    });
 }
