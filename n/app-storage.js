1201b8afc65042d9c247353c2cd8a082f3d6f526
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-10-22 15:58:51 +0900

    Update app-storage.js

--- n/app-storage.js
+++ n/app-storage.js
@@ -20,6 +20,7 @@
       static get observedAttributes() {
         return [
           "_index",
+          "_append",
           "_clear",
           "age",
           "artist",
@@ -29,18 +30,38 @@
         ];
       }
       attributeChangedCallback(name, oldValue, newValue) {
+        let indexStr = this.getAttribute("_index");
         if (name in this.__map) {
           localStorage.setItem(name, newValue);
         } else if (name === "_clear") {
           // TODO:
+        } else if (name === "_append") {
+          let maxStr = this.getAttribute("max");
+          let max = -1;
+          if (!maxStr) {
+            max = parseInt(maxStr, 10) + 1;
+          }
+          this.setAttribute("max", max);
+          this.setAttribute("_index", max);
+          this.setAttribute("_data", newValue);
         } else if (name === "_index") {
+          let maxStr = this.getAttribute("max");
+          let minStr = this.getAttribute("min");
+          if (newValue === "_next") {
+            indexStr = findDataIndex(indexStr, true, minStr, maxStr);
+          } else if (newValue === "_back") {
+            indexStr = findDataIndex(indexStr, false, minStr, maxStr);
+          } else if (newValue === "_min") {
+            indexStr = findDataIndex(minStr, true, minStr, maxStr);
+          } else if (newValue === "_max") {
+            indexStr = findDataIndex(maxStr, false, minStr, maxStr);
+          }
           this.setAttribute(
             "_data",
-            localStorage.getItem(this.__data + newValue)
+            localStorage.getItem(this.__data + indexStr)
           );
         } else if (name === "_data") {
-          let index = this.getAttribute("_index");
-          localStorage.setItem(this.__data + index, newValue);
+          localStorage.setItem(this.__data + indexStr, newValue);
         }
       }
       connectedCallback() {
@@ -55,9 +76,14 @@
         }
         let presetlist = this.querySelector("datalist");
         let base = this.getAttribute("base");
+        let max = null;
+        let min = 0;
         if (presetlist) {
           presetlist.querySelectorAll("option").forEach((o, i) => {
             let index = -1 - i;
+            if (min > index) {
+              min = index;
+            }
             let url = o.value;
             // TODO: support zip archive as base value
             if (base && !/^http/.test(url)) {
@@ -70,7 +96,46 @@
               });
           });
         }
+        this.setAttribute("min", min);
+        max = this.getAttribute("max");
+        if (max === null) {
+          this.setAttribute("max", -1);
+        }
+        this.setAttribute("_index",this.getAttribute("max"));
       }
     }
   );
+
+  function findDataIndex(indexStr, direct, minStr, maxStr) {
+    if (!minStr || !maxStr) {
+      return;
+    }
+    let index = parseInt(indexStr, 10);
+    let max = parseInt(maxStr, 10);
+    let min = parseInt(minStr, 10);
+    if (max < min) {
+      return;
+    }
+    if (index !== min || index !== max) {
+      if (direct) {
+        index += 1;
+      } else {
+        index -= 1;
+      }
+    }
+    let str = null;
+    while (min <= index && index <= max) {
+      this.setAttribute("_index", index);
+      str = storage.getAttribute("_data");
+      if (str) {
+        break;
+      }
+      if (direct) {
+        index++;
+      } else {
+        index--;
+      }
+    }
+    return index.toString();
+  }
 })();
