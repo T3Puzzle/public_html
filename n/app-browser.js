b2fabe427f081b1a7f6e57b5c147d365df4941bf
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-10-24 06:53:06 +0900

    Update app-browser.js

--- n/app-browser.js
+++ n/app-browser.js
@@ -1,3 +1,6 @@
+// TODO: dummy
+import { buffer } from "https://www.t3puzzle.com/n/buffer.module.js";
+
 (() => {
   customElements.define(
     "app-browser",
@@ -7,9 +10,11 @@
         this.__deck = this.querySelector("div.deck");
         // common
         this.__modal = this.querySelector("dialog-modal");
-        this.__grid = this.querySelector("grid-puzzle");
         this.__form = this.querySelector("form");
-        this.__src = this.querySelector("div.src");
+        //
+        this.__grid = this.querySelector("grid-puzzle");
+        let forName = this.__grid.getAttribute("for");
+        this.__src = this.querySelector(forName);
         //
         this.__name = this.getAttribute("name");
         this.__storage = document.querySelector(
@@ -17,7 +22,7 @@
         );
       }
       static get observedAttributes() {
-        return ["_open", "_go","_close"];
+        return ["_open", "_go", "_close"];
       }
       attributeChangedCallback(name, oldValue, newValue) {
         if (name === "_open") {
@@ -31,58 +36,62 @@
           );
         } else if (name === "_go") {
           go(
-            this.__storage, 
-            this.__grid, 
-            this.__form, 
+            (v) => this.__grid.setAttribute("_load_data", v),
+            this.__storage,
+            this.__form,
             this.__deck,
-            newValue);
+            newValue
+          );
         } else if (name === "_close") {
           close(
+            () => this.__modal.setAttribute("_close", ""),
             this,
-            this.__modal,
-            this.__storage);
+            this.__storage
+          );
         }
       }
     }
   );
-  function close(me,modal,storage) {
-    modal.setAttribute("_close", "");
+  function close(callback, me, storage) {
     me.value = storage.getAttribute("_data");
     let value = {
-            detail: {
-              value: me.value
-            }
-    }; 
-    me.dispatchEvent(new CustomEvent('change',value));
+      detail: {
+        value: me.value
+      }
+    };
+    me.dispatchEvent(new CustomEvent("change", value));
+    callback();
   }
-  function go(storage, grid, form, deck, action) {
+  function go(callback, storage, form, deck, action) {
     [storage.setAttribute("_go", action), storage.getAttribute("_data")]
       .filter((v) => v)
       .map((v) => {
-        grid.setAttribute("_load_data", v);
-        updateDeck(deck,storage,form,v);
+        updateDeck(deck, storage, form, v);
+        callback(v);
       });
   }
   function open(callback, storage, grid, form, src, deck) {
     let v = storage.getAttribute("_data");
     grid.setAttribute("_load_data", v);
-    updateDeck(deck,storage,form,v);
+    updateDeck(deck, storage, form, v);
     callback();
   }
-  function updateDeck (deck,storage,form,jsonStr) {
+  function updateDeck(deck, storage, form, jsonStr) {
     let json = JSON.parse(jsonStr);
     if (json && "meta" in json) {
-      ["title","note"]
-        .map(n=>({n,i:form.querySelector("[name="+n+"]")}))
-        .map(ni=>{ni.i.value = json.meta[ni.n]});
+      ["title", "note"]
+        .map((n) => ({ n, i: form.querySelector("[name=" + n + "]") }))
+        .map((ni) => {
+          ni.i.value = json.meta[ni.n];
+        });
     }
-    let min = parseInt(storage.getAttribute('min'),10);
-    let max = parseInt(storage.getAttribute('max'),10);
-    let idx = parseInt(storage.getAttribute('_index'),10);
+    let min = parseInt(storage.getAttribute("min"), 10);
+    let max = parseInt(storage.getAttribute("max"), 10);
+    let idx = parseInt(storage.getAttribute("_index"), 10);
     if (idx !== null) {
       let base = max - min + 1;
       let now = idx - min + 1;
-      deck.textContent = now + '/' + base;
+      deck.textContent = now + "/" + base;
     }
   }
 })();
