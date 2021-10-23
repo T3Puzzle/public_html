a6a12e95607f47d86ae25f7b75c01dc257c96466
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-10-23 13:21:57 +0900

    Update touchui.js

--- n/lib/touchui.js
+++ n/lib/touchui.js
@@ -1,4 +1,5 @@
 document.currentScript.insertAdjacentHTML('afterend',`
+<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <style>
     body {
       user-select: none;
