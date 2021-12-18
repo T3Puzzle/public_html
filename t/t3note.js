0eae8cc209214ce495ff9add02679e2a50f5a345
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2021-12-18 20:20:51 +0900

    Update t3note.js

--- t/t3note.js
+++ t/t3note.js
@@ -10,8 +10,9 @@ let sub = '/r/u?pop';
 let quest = 'https://docs.google.com/forms/d/e/1FAIpQLSev2VmJmDiQIW8K_XEQYNMk0XQJDrrzAs0IeFXDeXuqm1Ry_Q/viewform?usp=sf_link?usp=pp_url&entry.241675505='+artist;
 let text = `
 <a target="_blank" href="${url}">使い方</a> / 
-<a href="#" onclick="javascript:document.querySelector('a.pngview').click();return false;">画像に保存</a> /
-<a target="_blank" href="${sub}">画像を応募</a>
+<a href="#" onclick="javascript:document.querySelector('a.pngview').click();return false;">保存</a> /
+<a target="_blank" href="${sub}">応募</a> / 
+<a target="_blank" href="https://www.t3puzzle.com/b">最新版</a>
 `;
 document.addEventListener('DOMContentLoaded',()=>{
   let output = document.body.querySelector(`output.${config}`);
