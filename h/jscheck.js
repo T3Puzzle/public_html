de91dcb869d6c23e113502a337560b6fa680deb8
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2023-03-25 10:17:32 +0900

    Delete h directory

--- h/jscheck.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  try {
-    eval('e=>e');
-  } catch (e) {
-    document.write('<a href="https://drive.google.com/uc?export=download&id=120BSAbTIg8DljlSPPNav7Juj-dU5-KcE">Click to download</a>');
-  }
-})();
