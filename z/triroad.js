0d856b8a8a1f6d3ae5be1d4ab150178248b3bf14
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2023-12-28 12:20:43 +0900

    Update triroad.js

--- z/triroad.js
+++ z/triroad.js
@@ -4,20 +4,21 @@ import {
   isT3,
   drawT3,
   colorT3,
-  resetT3,
   arrangeT3,
   coord,
   toStr,
   parse,
-  getDxDy
-} from "libt3.js";
+  getDxDy,
+  dups,
+  getT3ByStr,
+  deleteT3ByStr,
+} from "https://codepen.io/alytile/pen/RwdPWjR.js";
 import {
   drawBackground,
   drawFrame,
   outOfFrame
-} from "libtriroad.js";
+} from "https://codepen.io/alytile/pen/YzgXyeq.js";
 
-const l_cst = getConsts();
 const j_paint = [];
 let j_count = 0;
 let j_target;
@@ -70,7 +71,8 @@ window.addEventListener("load", () => {
   };
   tool.onMouseDrag = function (event) {
     try {
-      let old = coord(j_lastEvent.point).ijk;
+      if (!j_lastEvent) return;
+      const old = coord(j_lastEvent.point).ijk;
       let { ijk, s } = coord(event.point);
       if (j_paint.length > 0) {
         // not to include outside frame
@@ -89,7 +91,7 @@ window.addEventListener("load", () => {
         }
         if (outOfFrame(ijk)) {
           colorT3(j_target, "red");
-        } else if (toStr(old) !== toStr(ijk) && l_cst.dup[toStr(ijk)]) {
+        } else if (toStr(old) !== toStr(ijk) && getT3ByStr(toStr(ijk))) {
           colorT3(j_target, "red");
         } else {
           colorT3(j_target, "green");
@@ -102,11 +104,11 @@ window.addEventListener("load", () => {
   };
   tool.onMouseUp = function (event) {
     try {
-      let old = coord(j_lastEvent.point).ijk;
+      if (!j_lastEvent) return;
+      const old = coord(j_lastEvent.point).ijk;
       let { ijk, s } = coord(event.point);
       if (j_paint.length > 0) {
         if (j_paint.length === 1 && j_paint[0] === toStr(ijk)) {
-          // triroad spepcific
           {
             if (j_count % 2) {
               s += 3;
@@ -117,47 +119,37 @@ window.addEventListener("load", () => {
         } else {
           // delete first
           for (let ai = 0; ai < j_paint.length; ai++) {
-            if (j_paint[ai] in l_cst.dup) {
-              l_cst.dup[j_paint[ai]].remove();
-              delete l_cst.dup[j_paint[ai]];
+            if (getT3ByStr(j_paint[ai])) {
+              deleteT3ByStr(j_paint[ai]);
             }
           }
           // draw next
           for (let ai = 0; ai < j_paint.length; ai++) {
             const t = parse(j_paint[ai]);
-            //TODO: determine s from j_paint sequence
-            if (l_cst.dup[j_paint[ai]]) {
-              l_cst.dup[j_paint[ai]].remove();
+            if (getT3ByStr(j_paint[ai])) {
+              deleteT3ByStr(j_paint[ai])
             }
+            //TODO: determine s from j_paint sequence
             drawT3(t, 1);
           }
         }
       } else {
         if (j_lastEvent.drag) {
           if (!j_target) return;
-          resetT3(j_target);
+          deleteT3ByStr(toStr(old));
           if (outOfFrame(ijk)) {
-            j_target.remove();
-            delete l_cst.dup[toStr(old)];
             return;
-          }
-          if (l_cst.dup[toStr(ijk)]) {
-            // move back to original place
-            ijk.i = old.i;
-            ijk.j = old.j;
-            ijk.k = old.k;
-          } else {
-            delete l_cst.dup[toStr(old)];
-            l_cst.dup[toStr(ijk)] = j_target;
           }
           if (j_target.data.flip) {
             s += 3;
           }
-          j_target.remove();
-          l_cst.dup[toStr(ijk)] = drawT3(ijk, s);
+          if (toStr(old) !== toStr(ijk) && getT3ByStr(toStr(ijk))) {
+            drawT3(old, s);
+          } else {
+            drawT3(ijk, s);
+          }
         } else {
-          const t3 = arrangeT3(j_lastEvent.item);
-          t3.bringToFront();
+          arrangeT3(j_lastEvent.item);
         }
       }
     } catch (e) {
