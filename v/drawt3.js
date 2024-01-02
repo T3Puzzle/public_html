f564cd1681fe1b031699070bd37aabca1dc86575
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2024-01-02 23:16:58 +0900

    Update drawt3.js

--- v/drawt3.js
+++ v/drawt3.js
@@ -5,7 +5,6 @@ import {
   isT3,
   drawT3,
   colorT3,
-  arrangeT3,
   coord,
   toStr,
   parse,
@@ -14,170 +13,287 @@ import {
   deleteT3ByStr,
   ghostT3
 } from "./libt3.js";
+import { addZoomHandler, resetZoom } from "./libzoom.js?xx";
 
+const j_zoom = {};
 const j_paint = [];
-let j_lastEvent;
-let j_white = false;
-let j_top = false;
-let j_m0 = null, j_m1=null;
-let j_last = new paper.Matrix(1,0,0,1,0,0);
-let j_multitouching = false;
+const j_point = [];
+let j_down;
+const l_cst = getConsts();
+
+function getIJRange(paint) {
+  let i_max = -1000,
+    i_min = 1000,
+    j_max = -1000,
+    j_min = 1000;
+  for (let pi = 0; pi < paint.length; pi++) {
+    const ijk = parse(paint[pi]);
+    if (ijk.i > i_max) i_max = ijk.i;
+    if (ijk.i < i_min) i_min = ijk.i;
+    if (ijk.j > j_max) j_max = ijk.j;
+    if (ijk.j < j_min) j_min = ijk.j;
+  }
+  return { i_min, i_max, j_min, j_max };
+}
+function mkPath(point) {
+  let myPath = new paper.Path();
+  myPath.strokeColor = "red";
+  myPath.strokeWidth = 10;
+  for (let pi = 0; pi < point.length; pi++) {
+    myPath.add(point[pi]);
+  }
+  return myPath;
+}
+function drawFrame(range, path,fill) {
+  let ret = [];
+  let all = [];
+  for (let i = range.i_min; i < range.i_max + 1; i++) {
+    for (let j = range.j_min; j < range.j_max + 1; j++) {
+      for (let k = 0; k < 2; k++) {
+        const frame = setFrame({ i, j, k });
+        all.push(frame);       
+        if (path.intersects(frame)) {
+          ret.push(toStr({ i, j, k }));
+          //if (fill) {
+          //  frame.fillColor = "#00ff00";
+          //}
+        } else if (fill) {
+          if (path.contains(tap({i,j,k}))) {
+            ret.push(toStr({ i, j, k }));
+            //frame.fillColor = "#0000ff";
+          }
+        }
+      }
+    }
+  }
+  for (let ai = 0; ai < all.length; ai++) {
+    all[ai].remove();
+  }
+  path.remove();
+  return ret;
+}
+function setFrame(ijk) {
+  const k = ijk.k;
+  const { dx, dy } = getDxDy(ijk);
+  const frame = new paper.Path.RegularPolygon({
+    center: [0, 0],
+    pivot: [0, 0],
+    sides: 3,
+    radius: l_cst.radius,
+    strokeColor: "#000000",
+    strokeWidth: 3,
+    strokeCap: "round",
+    opacity: 0.5
+  });
+  frame.scaling = 1;
+  frame.rotation = 30 + 180 * ((k + 1) % 2);
+  frame.position = [l_cst.offset.x + dx, l_cst.offset.y + dy];
+  return frame;
+}
+
+function getFill(paint,point) {
+  
+  if(paint.lenth===0){return;}
+  const {i_min,i_max,j_min,j_max} = getIJRange(paint);
+  const myPath = mkPath(point);
+
+  myPath.smooth();
+  return drawFrame({i_min,i_max,j_min,j_max},myPath,true);
+  
+}
+function neighboring(lp, p) {
+  try {
+    const lt = parse(lp);
+    const t = parse(p);
+    if (lt.k === t.k) {
+      return false;
+    } else {
+      if (lt.i === t.i && lt.j === t.j) {
+        return true;
+      } else if (lt.i === t.i && (lt.j+lt.k) === (t.j+t.k)) {
+        return true;
+      } else if (lt.j=== t.j && (lt.i+lt.k) === (t.i+t.k)) {
+        return true;
+      }
+      return false;
+    }
+  } catch (e) {
+    console.log("nei:" + e);
+  }
+}
+function findNext(now, array, out, put) {
+  try {
+    for (let ai = 0; ai < array.length; ai++) {
+      const next = array[ai];
+      if (next === now) continue;
+      // TODO:
+      if (out.indexOf(next) !== -1) continue;
+      if (neighboring(now, next)) {
+        out.push(next);
+        put.push(tap(parse(next)));
+        return next;
+      }
+    }
+    return null;
+  } catch (e) {
+    console.log("find:" + e);
+  }
+}
+function interpolatePaint(pa, po, out, put) {
+  try {
+    if (neighboring(pa.l, pa.n)) {
+      out.push(pa.n);
+      put.push(po.n);
+      return true;
+    }
+    const { i_min, i_max, j_min, j_max } = getIJRange([pa.l, pa.n]);
+    const myPath = mkPath([po.l, po.n]);
+    const array = drawFrame({ i_min, i_max, j_min, j_max }, myPath,false);
+    let next = findNext(pa.l, array, out,put);
+    let cnt=0;
+    const max=100;
+    while (true) {
+      if (cnt>max) break;
+      if (next === pa.n) break;
+      if (!next) break;
+      next = findNext(next, array,  out, put);
+      cnt++;
+    }
+    return next === pa.n;
+  } catch (e) {
+    console.log("in:" + e);
+  }
+  return false;
+}
+function processShadow(pa, po, paint, point) {
+  interpolatePaint(pa, po, paint, point);
+  for (let pi = 0; pi < paint.length; pi++) {
+    setShadow(parse(paint[pi]), getShadowColor());
+  }
+}
+function tap(ijk) {
+  const SQ3 = Math.sqrt(3);
+  const xbase = (l_cst.radius * 3) / 2;
+  const ybase = l_cst.radius * SQ3;
+  let x = l_cst.offset.x + xbase * ijk.j;
+  let y = l_cst.offset.y + ybase * ijk.i + (ybase * ijk.j) / 2;
+  if (ijk.k === 1) {
+    x += xbase / 3;
+    y += ybase / 2;
+  }
+  return [x, y];
+}
+function processPaint(paint, point, down) {
+  let change = [];
+  for (let ai = 1; ai < paint.length; ai++) {
+    const lt = parse(paint[ai - 1]);
+    const t = parse(paint[ai]);
+    if (lt.k !== t.k) {
+      if (lt.i === t.i && lt.j === t.j) {
+        change.push("R");
+      } else if (lt.j === t.j && Math.abs(lt.i - t.i) === 1) {
+        change.push("L");
+      } else if (lt.i === t.i && Math.abs(lt.j - t.j) === 1) {
+        change.push("M");
+      } else {
+        change.push("X");
+      }
+    } else {
+      change.push("X");
+    }
+  }
+  let circle = false;
+  let circlev = 0;
+  let color = [];
+  if (change.length === 1) {
+    if (change[0] === "L") {
+      color = [2, 2];
+    } else if (change[0] === "M") {
+      color = [1, 1];
+    } else if (change[0] === "R") {
+      color = [0, 0];
+    }
+  } else {
+    for (let ci = 1; ci < change.length; ci++) {
+      if ((change[ci - 1] + change[ci]).indexOf("R") == -1) {
+        color.push(2);
+      } else if ((change[ci - 1] + change[ci]).indexOf("L") == -1) {
+        color.push(1);
+      } else if ((change[ci - 1] + change[ci]).indexOf("M") == -1) {
+        color.push(0);
+      }
+    }
+    color.unshift(color[0]);
+
+    if (color.length >= 3) {
+      let c3 = color[color.length - 3];
+      let cc = [
+        change[change.length - 1],
+        change[change.length - 2],
+        change[change.length - 3]
+      ];
+      cc.sort();
+      // circle
+      if (cc.sort().join("") === "LMR") {
+        circle = true;
+        circlev = c3;
+      }
+    }
+    if (circle) {
+      color.push(circlev);
+    } else {
+      color.push(color[color.length - 1]);
+    }
+  }
+  let mod = 0;
+  if (circle) {
+    if (down.white !== down.top) {
+      mod = 3;
+    }
+  } else {
+    if (down.white === down.top) {
+      mod = 3;
+    }
+  }
+  for (let ai = 0; ai < paint.length; ai++) {
+    deleteT3ByStr(paint[ai]);
+    if (ai > 0) {
+      if (color[ai - 1] === color[ai]) {
+        mod = (mod + 3) % 6;
+      }
+    }
+    drawT3(parse(paint[ai]), color[ai] + mod, getT3Color());
+  }
+}
 
 window.addEventListener("load", () => {
   paper.setup(h_canvas);
   const tool = new paper.Tool();
   setBackground();
-  drawT3({ i: 3, j: 3, k: 0 }, 2,getT3Color());
-  drawT3({ i: 3, j: 2, k: 1 }, 5,getT3Color());
+  drawT3({ i: 3, j: 3, k: 0 }, 2, getT3Color());
+  drawT3({ i: 3, j: 2, k: 1 }, 5, getT3Color());
   paper.view.rotation = 30;
-  
+
   h_rotate.callback = function () {
     paper.view.rotation += 30;
   };
-  h_canvas.getCTM = function () {
-    return j_last;
-  }
-  h_canvas.callback = function (multi,type,m0,m1) {
-      if (m0!==null) {
-       j_m0 = new paper.Matrix(m0.a,m0.b,m0.c,m0.d,m0.tx,m0.ty);
-      }
-      if (m1!==null) {
-        j_m1 = new paper.Matrix(m1.a,m1.b,m1.c,m1.d,m1.tx,m1.ty);
-      }
-      let g = j_m0.appended(j_m1);
-      paper.view.matrix = g;
-      j_last = g;
-  }
-  // zoom/pan/rotate
-  if ('ontouchstart' in window) {
-    const offset = {
-      r: 0,
-      s: 1
-    };
-    h_canvas.addEventListener('touchstart', e => {
-     try {
-      if (e.touches.length !== 2) return;
-      {
-        j_multitouching = true;
-        for (let ai = 0; ai < j_paint.length; ai++) {
-          deleteShadowByStr(j_paint[ai]);
-        }
-        j_paint.length=0;
-        j_lastEvent = null;
-      }
-      e.preventDefault();
-      const u = update(e, null);
-      Object.assign(offset, {
-        r: u.r,
-        s: u.s
-      });
-      const m = h_canvas.getCTM();
-      h_canvas.callback(true, 'touchstart', {
-        a: 1,
-        b: 0,
-        c: 0,
-        d: 1,
-        tx: u.cx,
-        ty: u.cy
-      }, {
-        a: m.a,
-        b: m.b,
-        c: m.c,
-        d: m.d,
-        tx: m.tx - u.cx,
-        ty: m.ty - u.cy
-      });
-      } catch (e) {
-        h_warn.textContent = 'touchStart: '+e;
-      }
-    });
-    h_canvas.addEventListener('touchmove', e => {
-      try {
-      if (e.touches.length !== 2) return;
-      e.preventDefault();
-      const u = update(e, offset);
-      let tt = u.r * Math.PI / 180;
-      let cc = Math.cos(tt);
-      let ss = Math.sin(tt);
-      let aa = u.s * cc;
-      let bb = u.s * ss;
-      h_canvas.callback(true, 'touchmove', {
-        a: aa,
-        b: bb,
-        c: -bb,
-        d: aa,
-        tx: u.cx,
-        ty: u.cy
-      }, null);      
-      } catch (e) {
-        h_warn.textContent = 'touchMove: '+e;
-      }
-    });
-    h_canvas.addEventListener('touchend', e => {
-      try {
-      if (!j_multitouching) return;
-      Object.assign(offset, {
-        r: 0,
-        s: 1
-      });
-      const m = h_canvas.getCTM();
-      h_canvas.callback(true, 'touchend', m, {
-        a: 1,
-        b: 0,
-        c: 0,
-        d: 1,
-        tx: 0,
-        ty: 0
-      });
-      j_multitouching = false;
-      } catch (e) {
-        h_warn.textContent = 'touchEnd: '+e;
-      }
-   });
+  h_view.callback = function () {
+    resetZoom();
+    paper.view.rotation = 30;
+  };
 
-    function update(e, offset) {
-      const cx = (e.touches[0].pageX + e.touches[1].pageX) / 2;
-      const cy = (e.touches[0].pageY + e.touches[1].pageY) / 2;
-      const o = {
-        r: 0,
-        s: 1
-      };
-      offset ? Object.assign(o, {
-        r: offset.r,
-        s: offset.s
-      }) : null;
-      const dx = e.touches[1].pageX - e.touches[0].pageX;
-      const dy = e.touches[1].pageY - e.touches[0].pageY;
-      const r = Math.atan2(dy, dx) * 180 / Math.PI - o.r;
-      const s = Math.sqrt(dx * dx + dy * dy) / o.s;
-      return {
-        cx,
-        cy,
-        r,
-        s
-      };
+  addZoomHandler(h_canvas, j_zoom, () => {
+    for (let ai = 0; ai < j_paint.length; ai++) {
+      deleteShadowByStr(j_paint[ai]);
     }
-  }  
-  document.addEventListener(
-    "mousewheel",
-    (event) => {
-      const oldZoom = paper.view.zoom;
-      let beta = 0.95;
-      if (event.deltaY > 0) beta = 1.05;
-      let mpos = paper.view.viewToProject([event.offsetX, event.offsetY]);
-      let ctr = paper.view.center;
-      let pc = mpos.subtract(ctr);
-      paper.view.zoom /= beta;
-      paper.view.center = mpos.subtract(pc.multiply(beta));
-      event.preventDefault();
-    },
-    {
-      passive: false
-    }
-  );
+    j_paint.length = 0;
+    j_point.length = 0;
+    j_down = null;
+  });
+
   tool.onMouseDown = function (event) {
     try {
-      j_lastEvent = null;
+      j_down = null;
       let hitResult = paper.project.hitTest(event.point, {
         fill: true,
         tolerance: 5,
@@ -187,178 +303,81 @@ window.addEventListener("load", () => {
       if (!hitResult) return;
       let { ijk, s } = coord(event.point);
       j_paint.length = 0;
-      j_lastEvent = {
-        drag: false,
-        item: hitResult.item
-      };
-      j_top = !!isT3(hitResult.item, "top");
-      j_white = j_lastEvent.item.fillColor.red === 1;
+      j_point.length = 0;
       if (!isT3(hitResult.item)) {
         j_paint.push(toStr(ijk));
+        j_point.push(event.point);
       }
+      j_down = {
+        drag: false,
+        top: !!isT3(hitResult.item, "top"),
+        center: !!isT3(hitResult.item, "center"),
+        left: !!isT3(hitResult.item, "left"),
+        right: !!isT3(hitResult.item, "right"),
+        white:
+          hitResult.item.fillColor.red === 1 &&
+          hitResult.item.fillColor.green === 1 &&
+          hitResult.item.fillColor.blue === 1
+      };
     } catch (e) {
       h_warn.textContent = "down:" + e;
     }
   };
   tool.onMouseDrag = function (event) {
     try {
-      if (!j_lastEvent) return;
-      if (j_multitouching) return;
-      {
-        let { ijk, s } = coord(event.point);
-        if (j_paint.length > 0) {
-          if (j_paint[j_paint.length - 1] !== toStr(ijk)) {
-            if (j_paint.length === 1) {
-              setShadow(parse(j_paint[0]), getShadowColor());
-            }
-            j_paint.push(toStr(ijk));
-            setShadow(ijk, getShadowColor());
-          }
-        } else {
-          j_paint.push(toStr(ijk));
+      if (!j_down) return;
+      if (j_zoom.multitouching) return;
+      let { ijk, s } = coord(event.point);
+      if (j_paint.length > 0) {
+        if (j_paint[j_paint.length - 1] !== toStr(ijk)) {
+          processShadow(
+            { l: j_paint[j_paint.length - 1], n: toStr(ijk) },
+            { l: j_point[j_point.length - 1], n: event.point },
+            j_paint,
+            j_point
+          );
         }
+      } else {
+        j_paint.push(toStr(ijk));
+        j_point.push(event.point);
       }
-      j_lastEvent.drag = true;
+      j_down.drag = true;
     } catch (e) {
       h_warn.textContent = "drag:" + e;
     }
   };
   tool.onMouseUp = function (event) {
     try {
-      if (!j_lastEvent) return;
+      if (!j_down) return;
       let { ijk, s } = coord(event.point);
       const mode = getMode();
       // just in case
       for (let ai = 0; ai < j_paint.length; ai++) {
         deleteShadowByStr(j_paint[ai]);
       }
-      
-      if (j_multitouching) return;
-      
+      if (j_zoom.multitouching) return;
       if (mode === "Eraser") {
-        for (let ai = 0; ai < j_paint.length; ai++) {
-          deleteT3ByStr(j_paint[ai]);
+        const to_delete = getFill(j_paint,j_point);
+        for (let ai = 0; ai < to_delete.length; ai++) {
+          deleteT3ByStr(to_delete[ai]);
         }
       } else {
-        if (j_paint.length === 0) {
-          arrangeT3(j_lastEvent.item);
+        if (!j_down.drag) {
+          if (j_down.center) {
+            deleteT3ByStr(toStr(ijk));
+          } else {
+            if (j_down.white) {
+              drawT3(ijk, s + 3, getT3Color());
+            } else {
+              drawT3(ijk, s, getT3Color());
+            }
+          }
         } else {
           if (j_paint.length === 1 && j_paint[0] === toStr(ijk)) {
+            // just little drag
             drawT3(ijk, s, getT3Color());
           } else {
-            let change = [];
-            let pnt = [j_paint[0]];
-            for (let ai = 1; ai < j_paint.length; ai++) {
-              const lt = parse(j_paint[ai - 1]);
-              const t = parse(j_paint[ai]);
-              if (lt.k !== t.k) {
-                pnt.push(j_paint[ai]);
-              } else {
-                if (t.k === 0) {
-                  if (lt.j - t.j === 1) {
-                    pnt.push(toStr({ i: lt.i, j: lt.j - 1, k: 1 }));
-                  } else if (lt.i - t.i === 1) {
-                    pnt.push(toStr({ i: lt.i - 1, j: lt.j, k: 1 }));
-                  } else if (
-                    (lt.i === t.i && t.j - lt.j === 1) ||
-                    (lt.j === t.j && t.i - lt.i === 1)
-                  ) {
-                    pnt.push(toStr({ i: lt.i, j: lt.j, k: 1 }));
-                  }
-                } else {
-                  if (t.j - lt.j === 1) {
-                    pnt.push(toStr({ i: lt.i, j: lt.j + 1, k: 0 }));
-                  } else if (t.i - lt.i === 1) {
-                    pnt.push(toStr({ i: lt.i + 1, j: lt.j, k: 0 }));
-                  } else if (
-                    (t.i === lt.i && lt.j - t.j === 1) ||
-                    (t.j === lt.j && lt.i - t.i === 1)
-                  ) {
-                    pnt.push(toStr({ i: lt.i, j: lt.j, k: 0 }));
-                  }
-                }
-                pnt.push(j_paint[ai]);
-              }
-            }
-            for (let ai = 1; ai < pnt.length; ai++) {
-              const lt = parse(pnt[ai - 1]);
-              const t = parse(pnt[ai]);
-              if (lt.k !== t.k) {
-                if (lt.i === t.i && lt.j === t.j) {
-                  change.push("R");
-                } else if (lt.j === t.j && Math.abs(lt.i - t.i) === 1) {
-                  change.push("L");
-                } else if (lt.i === t.i && Math.abs(lt.j - t.j) === 1) {
-                  change.push("M");
-                } else {
-                  change.push("X");
-                }
-              } else {
-                change.push("X");
-              }
-            }
-            let circle = false;
-            let circlev = 0;
-            let color = [];
-            if (change.length === 1) {
-              if (change[0] === "L") {
-                color = [2, 2];
-              } else if (change[0] === "M") {
-                color = [1, 1];
-              } else if (change[0] === "R") {
-                color = [0, 0];
-              }
-            } else {
-              for (let ci = 1; ci < change.length; ci++) {
-                if ((change[ci - 1] + change[ci]).indexOf("R") == -1) {
-                  color.push(2);
-                } else if ((change[ci - 1] + change[ci]).indexOf("L") == -1) {
-                  color.push(1);
-                } else if ((change[ci - 1] + change[ci]).indexOf("M") == -1) {
-                  color.push(0);
-                }
-              }
-              color.unshift(color[0]);
-
-              if (color.length >= 3) {
-                let c3 = color[color.length - 3];
-                let cc = [
-                  change[change.length - 1],
-                  change[change.length - 2],
-                  change[change.length - 3]
-                ];
-                cc.sort();
-                // circle
-                if (cc.sort().join("") === "LMR") {
-                  circle = true;
-                  circlev = c3;
-                }
-              }
-              if (circle) {
-                color.push(circlev);
-              } else {
-                color.push(color[color.length - 1]);
-              }
-            }
-            let mod = 0;
-            if (circle) {
-              if (j_white !== j_top) {
-                mod = 3;
-              }
-            } else {
-              if (j_white === j_top) {
-                mod = 3;
-              }
-            }
-            for (let ai = 0; ai < pnt.length; ai++) {
-              deleteT3ByStr(pnt[ai]);
-              if (ai > 0) {
-                if (color[ai - 1] === color[ai]) {
-                  mod = (mod + 3) % 6;
-                }
-              }
-              drawT3(parse(pnt[ai]), color[ai] + mod, getT3Color());
-            }
+            processPaint(j_paint, j_point, j_down);
           }
         }
         return;
@@ -395,7 +414,7 @@ function getShadowColor() {
     return "red";
   }
 }
-function getT3Color () {
+function getT3Color() {
   return window.getComputedStyle(h_color).getPropertyValue("color");
   //return '#59BCE0';
 }
