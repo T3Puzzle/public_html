6735005977791d21fc44bf078b01c48108092a00
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2024-01-03 14:49:01 +0900

    Update drawt3.js

--- v/drawt3.js
+++ v/drawt3.js
@@ -13,7 +13,10 @@ import {
   deleteT3ByStr,
   ghostT3
 } from "./libt3.js";
-import { addZoomHandler, resetZoom } from "./libzoom.js?xx";
+import {
+  addZoomHandler,
+  resetZoom
+} from "./libzoom.js";
 
 const j_zoom = {};
 const j_paint = [];
@@ -44,21 +47,21 @@ function mkPath(point) {
   }
   return myPath;
 }
-function drawFrame(range, path,fill) {
+function drawFrame(range, path, fill) {
   let ret = [];
   let all = [];
   for (let i = range.i_min; i < range.i_max + 1; i++) {
     for (let j = range.j_min; j < range.j_max + 1; j++) {
       for (let k = 0; k < 2; k++) {
         const frame = setFrame({ i, j, k });
-        all.push(frame);       
+        all.push(frame);
         if (path.intersects(frame)) {
           ret.push(toStr({ i, j, k }));
           //if (fill) {
           //  frame.fillColor = "#00ff00";
           //}
         } else if (fill) {
-          if (path.contains(tap({i,j,k}))) {
+          if (path.contains(tap({ i, j, k }))) {
             ret.push(toStr({ i, j, k }));
             //frame.fillColor = "#0000ff";
           }
@@ -90,17 +93,17 @@ function setFrame(ijk) {
   frame.position = [l_cst.offset.x + dx, l_cst.offset.y + dy];
   return frame;
 }
-
-function getFill(paint,point) {
-  
-  if(paint.lenth===0){return;}
-  const {i_min,i_max,j_min,j_max} = getIJRange(paint);
+function getFill(paint, point) {
+  if (paint.lenth === 0) {
+    return;
+  }
+  const { i_min, i_max, j_min, j_max } = getIJRange(paint);
   const myPath = mkPath(point);
 
   myPath.smooth();
-  return drawFrame({i_min,i_max,j_min,j_max},myPath,true);
-  
+  return drawFrame({ i_min, i_max, j_min, j_max }, myPath, true);
 }
+
 function neighboring(lp, p) {
   try {
     const lt = parse(lp);
@@ -110,9 +113,9 @@ function neighboring(lp, p) {
     } else {
       if (lt.i === t.i && lt.j === t.j) {
         return true;
-      } else if (lt.i === t.i && (lt.j+lt.k) === (t.j+t.k)) {
+      } else if (lt.i === t.i && lt.j + lt.k === t.j + t.k) {
         return true;
-      } else if (lt.j=== t.j && (lt.i+lt.k) === (t.i+t.k)) {
+      } else if (lt.j === t.j && lt.i + lt.k === t.i + t.k) {
         return true;
       }
       return false;
@@ -148,15 +151,15 @@ function interpolatePaint(pa, po, out, put) {
     }
     const { i_min, i_max, j_min, j_max } = getIJRange([pa.l, pa.n]);
     const myPath = mkPath([po.l, po.n]);
-    const array = drawFrame({ i_min, i_max, j_min, j_max }, myPath,false);
-    let next = findNext(pa.l, array, out,put);
-    let cnt=0;
-    const max=100;
+    const array = drawFrame({ i_min, i_max, j_min, j_max }, myPath, false);
+    let next = findNext(pa.l, array, out, put);
+    let cnt = 0;
+    const max = 100;
     while (true) {
-      if (cnt>max) break;
+      if (cnt > max) break;
       if (next === pa.n) break;
       if (!next) break;
-      next = findNext(next, array,  out, put);
+      next = findNext(next, array, out, put);
       cnt++;
     }
     return next === pa.n;
@@ -183,8 +186,27 @@ function tap(ijk) {
   }
   return [x, y];
 }
-function processPaint(paint, point, down) {
+function processPaint(paint, point, down, up) {
   let change = [];
+  let trimExtrapolated = false;
+  // extrapolate
+  if (point.length === 2) {
+    const dx= (point[1].x - point[0].x);
+    const dy=(point[1].y - point[0].y);
+    const norm = Math.sqrt(dx*dx+dy*dy);
+    const extra= l_cst.radius*2/norm;
+    const _p = {
+      x: point[1].x + extra * dx,
+      y: point[1].y + extra * dy
+    };
+    interpolatePaint(
+      { l: paint[paint.length - 1], n: toStr(coord(_p).ijk) },
+      { l: point[point.length - 1], n: _p },
+      paint,
+      point
+    );
+    trimExtrapolated = point.length > 2;
+  }
   for (let ai = 1; ai < paint.length; ai++) {
     const lt = parse(paint[ai - 1]);
     const t = parse(paint[ai]);
@@ -205,73 +227,116 @@ function processPaint(paint, point, down) {
   let circle = false;
   let circlev = 0;
   let color = [];
-  if (change.length === 1) {
-    if (change[0] === "L") {
-      color = [2, 2];
-    } else if (change[0] === "M") {
-      color = [1, 1];
-    } else if (change[0] === "R") {
-      color = [0, 0];
-    }
-  } else {
-    for (let ci = 1; ci < change.length; ci++) {
-      if ((change[ci - 1] + change[ci]).indexOf("R") == -1) {
-        color.push(2);
-      } else if ((change[ci - 1] + change[ci]).indexOf("L") == -1) {
-        color.push(1);
-      } else if ((change[ci - 1] + change[ci]).indexOf("M") == -1) {
-        color.push(0);
-      }
-    }
-    color.unshift(color[0]);
 
-    if (color.length >= 3) {
-      let c3 = color[color.length - 3];
-      let cc = [
-        change[change.length - 1],
-        change[change.length - 2],
-        change[change.length - 3]
-      ];
-      cc.sort();
-      // circle
-      if (cc.sort().join("") === "LMR") {
-        circle = true;
-        circlev = c3;
-      }
-    }
-    if (circle) {
-      color.push(circlev);
-    } else {
-      color.push(color[color.length - 1]);
+  for (let ci = 1; ci < change.length; ci++) {
+    if ((change[ci - 1] + change[ci]).indexOf("R") == -1) {
+      color.push(2);
+    } else if ((change[ci - 1] + change[ci]).indexOf("L") == -1) {
+      color.push(1);
+    } else if ((change[ci - 1] + change[ci]).indexOf("M") == -1) {
+      color.push(0);
     }
   }
-  let mod = 0;
+  color.unshift(color[0]);
+
+  if (color.length >= 3) {
+    let c3 = color[color.length - 3];
+    let cc = [
+      change[change.length - 1],
+      change[change.length - 2],
+      change[change.length - 3]
+    ];
+    cc.sort();
+    // circle
+    if (cc.sort().join("") === "LMR") {
+      circle = true;
+      circlev = c3;
+    }
+  }
   if (circle) {
-    if (down.white !== down.top) {
-      mod = 3;
-    }
+    color.push(circlev);
   } else {
-    if (down.white === down.top) {
-      mod = 3;
+    color.push(color[color.length - 1]);
+  }
+
+  let mod = 3;
+  if (trimExtrapolated) {
+    paint.length = 2;
+    point.length = 2;
+  }
+  let direct;
+  {
+    const lp = (paint[0]);
+    const p = (paint[1]);
+    const lpp = parse(lp);
+    const pp = parse(p);
+    
+    if ((lpp.j===pp.j)&&lpp.i===pp.i) {
+      if(lpp.k>pp.k) { direct = 'up';} else { direct = 'down';}
+    } else if (lpp.j===pp.j) {
+      if(lpp.i>pp.i) { direct = 'right';} else { direct = 'left';}
+    } else if (lpp.i===pp.i) {
+      if(lpp.j<pp.j) { direct = 'right';} else { direct = 'left';}
     }
+  }
+  const first_k = parse(paint[0]).k;
+  if (color[0] === 0) {
+    // left
+    if (direct==='left') mod +=3;
+    if (direct==='down') mod +=3;
+    if (first_k) mod += 3;
+  } else if (color[0] === 1) {
+    // right
+    if (first_k ) mod += 3;
+    if (direct==='right') mod +=3;
+    if (direct==='down') mod +=3;
+    console.log(direct);
+  } else if (color[0] === 2) {
+    // top
+    if (direct==='left') mod +=3;
+    if (first_k)  mod += 3;
   }
   for (let ai = 0; ai < paint.length; ai++) {
     deleteT3ByStr(paint[ai]);
     if (ai > 0) {
-      if (color[ai - 1] === color[ai]) {
+      // alternating neighboring colors
+      if (
+        color[ai - 1] === color[ai] &&
+        neighboring(paint[ai - 1], paint[ai])
+      ) {
         mod = (mod + 3) % 6;
       }
     }
-    drawT3(parse(paint[ai]), color[ai] + mod, getT3Color());
+    drawT3(parse(paint[ai]), color[ai] + mod%6, getT3Color());
   }
 }
-
+function hit(point) {
+  return paper.project.hitTest(point, {
+    fill: true,
+    tolerance: 5,
+    segments: false,
+    stroke: false
+  });
+}
+function detect(hitResult) {
+  let type = "top";
+  ["top", "center", "left", "right"].map((t) => {
+    if (isT3(hitResult.item, t)) type = t;
+  });
+  return {
+    drag: false,
+    type: type,
+    white:
+      hitResult.item.fillColor.red === 1 &&
+      hitResult.item.fillColor.green === 1 &&
+      hitResult.item.fillColor.blue === 1
+  };
+}
 window.addEventListener("load", () => {
   paper.setup(h_canvas);
   const tool = new paper.Tool();
   setBackground();
-  drawT3({ i: 3, j: 3, k: 0 }, 2, getT3Color());
-  drawT3({ i: 3, j: 2, k: 1 }, 5, getT3Color());
+  drawT3(coord(paper.view.center).ijk, 2, getT3Color());
   paper.view.rotation = 30;
 
   h_rotate.callback = function () {
@@ -281,7 +346,6 @@ window.addEventListener("load", () => {
     resetZoom();
     paper.view.rotation = 30;
   };
-
   addZoomHandler(h_canvas, j_zoom, () => {
     for (let ai = 0; ai < j_paint.length; ai++) {
       deleteShadowByStr(j_paint[ai]);
@@ -294,31 +358,18 @@ window.addEventListener("load", () => {
   tool.onMouseDown = function (event) {
     try {
       j_down = null;
-      let hitResult = paper.project.hitTest(event.point, {
-        fill: true,
-        tolerance: 5,
-        segments: false,
-        stroke: false
-      });
+      let hitResult = hit(event.point);
       if (!hitResult) return;
       let { ijk, s } = coord(event.point);
       j_paint.length = 0;
       j_point.length = 0;
-      if (!isT3(hitResult.item)) {
+      j_down = detect(hitResult);
+      // place new
+      j_down.first = !isT3(hitResult.item);
+      if (j_down.first) {
         j_paint.push(toStr(ijk));
         j_point.push(event.point);
       }
-      j_down = {
-        drag: false,
-        top: !!isT3(hitResult.item, "top"),
-        center: !!isT3(hitResult.item, "center"),
-        left: !!isT3(hitResult.item, "left"),
-        right: !!isT3(hitResult.item, "right"),
-        white:
-          hitResult.item.fillColor.red === 1 &&
-          hitResult.item.fillColor.green === 1 &&
-          hitResult.item.fillColor.blue === 1
-      };
     } catch (e) {
       h_warn.textContent = "down:" + e;
     }
@@ -351,33 +402,59 @@ window.addEventListener("load", () => {
       if (!j_down) return;
       let { ijk, s } = coord(event.point);
       const mode = getMode();
-      // just in case
+      // TODO: just in case
       for (let ai = 0; ai < j_paint.length; ai++) {
         deleteShadowByStr(j_paint[ai]);
       }
       if (j_zoom.multitouching) return;
+      const white = j_down.white ? 3 : 0;
       if (mode === "Eraser") {
-        const to_delete = getFill(j_paint,j_point);
+        const to_delete = getFill(j_paint, j_point);
         for (let ai = 0; ai < to_delete.length; ai++) {
           deleteT3ByStr(to_delete[ai]);
         }
       } else {
         if (!j_down.drag) {
-          if (j_down.center) {
+          if (j_down.type === "center") {
             deleteT3ByStr(toStr(ijk));
           } else {
-            if (j_down.white) {
-              drawT3(ijk, s + 3, getT3Color());
-            } else {
-              drawT3(ijk, s, getT3Color());
-            }
+            drawT3(ijk, s + white, getT3Color());
           }
         } else {
           if (j_paint.length === 1 && j_paint[0] === toStr(ijk)) {
             // just little drag
-            drawT3(ijk, s, getT3Color());
+            const up = detect(hit(event.point));
+            if (j_down.type === up.type) {
+              // do nothing
+            } else if (j_down.type == "top") {
+              if (up.type === "left") {
+                drawT3(ijk, ((s + 1) % 3) + white, getT3Color());
+              } else if (up.type === "right") {
+                drawT3(ijk, ((s + 2) % 3) + white, getT3Color());
+              } else {
+                // do nothing
+              }
+            } else if (j_down.type == "left") {
+              if (up.type === "right") {
+                drawT3(ijk, ((s + 1) % 3) + white, getT3Color());
+              } else if (up.type === "top") {
+                drawT3(ijk, ((s + 2) % 3) + white, getT3Color());
+              } else {
+                // do nothing
+              }
+            } else if (j_down.type == "right") {
+              if (up.type === "top") {
+                drawT3(ijk, ((s + 1) % 3) + white, getT3Color());
+              } else if (up.type === "left") {
+                drawT3(ijk, ((s + 2) % 3) + white, getT3Color());
+              } else {
+                // do nothing
+              }
+            } else {
+              // do nothing
+            }
           } else {
-            processPaint(j_paint, j_point, j_down);
+            processPaint(j_paint, j_point, j_down, detect(hit(event.point)));
           }
         }
         return;
