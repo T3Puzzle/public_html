78f939665914b6aa47798760db615de8f93ece87
T3Puzzle <40909109+T3Puzzle@users.noreply.github.com>
2024-01-03 23:33:33 +0900

    Update libt3.js

--- v/libt3.js
+++ v/libt3.js
@@ -46,7 +46,7 @@ function getT3ByStr (str) {
 }
 function getConsts() {
   const radius = 50;
-  const offset = { x: 100, y: 50 };
+  const offset = { x: 200, y: 50 };
   const width = parseInt(
     window.getComputedStyle(h_canvas).getPropertyValue("width"),
     10
@@ -154,6 +154,7 @@ function setT3(ijk, s, opacity,scale,color) {
   t3c.onDoubleClick = function (event) {
     deleteT3ByStr(toStr(coord(event.point).ijk));
   };
+  
   return grp;
 }
 function ghostT3(ijk,s,scale) {
@@ -161,6 +162,8 @@ function ghostT3(ijk,s,scale) {
 }
 function drawT3(ijk, s,color) {
   const grp = setT3(ijk,s,1,1,color);
+  grp.data.s = s;
+  grp.data.color = color;
   if (toStr(ijk) in DUP) {
     DUP[toStr(ijk)].remove();
   }
